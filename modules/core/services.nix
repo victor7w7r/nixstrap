3f47a22fd467777cddc66514e9ee680bb012806d
victor7w7r <arkano036@gmail.com>
2025-11-28 19:22:16 -0500

    AddSSHService

--- modules/core/services.nix
+++ modules/core/services.nix
@@ -4,6 +4,7 @@
     gvfs.enable = true;
     displayManager.ly.enable = true;
     udisks2.enable = true;
+    openssh.enable = true;
     #gnome.gnome-keyring.enable = true;
     dbus.enable = true;
     fstrim.enable = true;
