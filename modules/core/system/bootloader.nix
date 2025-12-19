7755ad858463f4eebf016e793df51726fa64cffe
victor7w7r <arkano036@gmail.com>
2025-12-18 22:03:43 -0500

    AddBootLoaderPkgs

--- modules/core/system/bootloader.nix
+++ modules/core/system/bootloader.nix
@@ -9,20 +9,20 @@
   system.build.installBootLoader = pkgs.writeScript "installBootLoader.sh" ''
     #!${pkgs.bash}/bin/bash
 
-    mkdir -p /boot/EFI/tools /boot/EFI/BOOT /boot/EFI/refind/themes
+    ${pkgs.coreutils}/bin/mkdir -p /boot/EFI/tools /boot/EFI/BOOT /boot/EFI/refind/themes
 
-    cp ${pkgs.refind}/share/refind/refind_x64.efi /boot/EFI/refind/refind_x64.efi
-    cp ${pkgs.refind}/share/refind/refind_x64.efi /boot/EFI/BOOT/BOOTX64.efi
-    cp -r ${pkgs.refind}/share/refind/drivers_x64 /boot/EFI/refind/drivers_x64
-    cp -r ${pkgs.refind}/share/refind/icons /boot/EFI/refind/icons
-    cp -r ${pkgs.refind}/share/refind/fonts /boot/EFI/refind/fonts
+    ${pkgs.coreutils}/bin/cp ${pkgs.refind}/share/refind/refind_x64.efi /boot/EFI/refind/refind_x64.efi
+    ${pkgs.coreutils}/bin/cp ${pkgs.refind}/share/refind/refind_x64.efi /boot/EFI/BOOT/BOOTX64.efi
+    ${pkgs.coreutils}/bin/cp -r ${pkgs.refind}/share/refind/drivers_x64 /boot/EFI/refind/drivers_x64
+    ${pkgs.coreutils}/bin/cp -r ${pkgs.refind}/share/refind/icons /boot/EFI/refind/icons
+    ${pkgs.coreutils}/bin/cp -r ${pkgs.refind}/share/refind/fonts /boot/EFI/refind/fonts
 
-    cp ${pkgs.edk2-uefi-shell}/shell.efi /boot/EFI/tools/shellx64.efi
-    cp ${pkgs.memtest86-efi}/BOOTX64.efi /boot/EFI/tools/memtest86.efi
-    cp ${pkgs.fwupd-efi}/libexec/fwupd/efi/fwupdx64.efi /boot/EFI/tools/fwupx64.efi
-    cp -r ${inputs.catppuccin-refind} /boot/EFI/refind/themes/catppuccin
+    ${pkgs.coreutils}/bin/cp ${pkgs.edk2-uefi-shell}/shell.efi /boot/EFI/tools/shellx64.efi
+    ${pkgs.coreutils}/bin/cp ${pkgs.memtest86-efi}/BOOTX64.efi /boot/EFI/tools/memtest86.efi
+    ${pkgs.coreutils}/bin/cp ${pkgs.fwupd-efi}/libexec/fwupd/efi/fwupdx64.efi /boot/EFI/tools/fwupx64.efi
+    ${pkgs.coreutils}/bin/cp -r ${inputs.catppuccin-refind} /boot/EFI/refind/themes/catppuccin
 
-    cat > /boot/EFI/refind/refind.conf << EOF
+    ${pkgs.coreutils}/bin/cat > /boot/EFI/refind/refind.conf << EOF
       timeout 2
       screensaver 300
       use_nvram false
@@ -58,11 +58,12 @@
       }
     EOF
 
-    EFI_INFO=$(lsblk -o NAME,PARTTYPE,PKNAME,PARTTYPENAME,FSTYPE | grep -i "EFI" | grep -i "vfat" | head -n1)
+    EFI_INFO=$(${pkgs.util-linux}/bin/lsblk -o NAME,PARTTYPE,PKNAME,PARTTYPENAME,FSTYPE | \
+      ${pkgs.gnugrep}/bin/grep -i "EFI" | ${pkgs.gnugrep}/bin/grep -i "vfat" | ${pkgs.coreutils}/bin/head -n1)
     DISK=$(echo "$EFI_INFO" | ${pkgs.gawk}/bin/awk '{print $3}')
 
     ${pkgs.efibootmgr}/bin/efibootmgr \
-      | grep -i "rEFind" \
+      | ${pkgs.gnugrep}/bin/grep -i "rEFind" \
       | ${pkgs.gawk}/bin/awk '{print $1}' \
       | ${pkgs.gnused}/bin/sed 's/Boot//' \
       | ${pkgs.gnused}/bin/sed 's/\*//' \
@@ -76,7 +77,7 @@
       --unicode &> /dev/null
 
     TOPLEVEL=$1
-    BASE=$(basename $TOPLEVEL)
+    BASE=$(${pkgs.coreutils}/bin/basename $TOPLEVEL)
     ${pkgs.buildPackages.systemdUkify}/lib/systemd/ukify build \
       --linux="${config.boot.kernelPackages.kernel}/${config.system.boot.loader.kernelFile}" \
       --initrd="${config.system.build.initialRamdisk}/${config.system.boot.loader.initrdFile}" \
@@ -88,7 +89,7 @@
       #--secureboot-private-key /var/lib/sbctl/db/db.key \
       #--secureboot-certificate /var/lib/sbctl/db/db.pem \
 
-    cp /boot/EFI/nixos.efi /boot/vault/$BASE.efi
+    ${pkgs.coreutils}/bin/cp /boot/EFI/nixos.efi /boot/vault/$BASE.efi
     echo "$BASE" > /boot/EFI/nixos.txt
 
     #if command -v sbctl >/dev/null 2>&1; then
