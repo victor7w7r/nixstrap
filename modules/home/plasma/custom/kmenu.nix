7e62e7bd8a6a2e16dca4cd60c97553c84126460a
victor7w7r <arkano036@gmail.com>
2025-12-20 21:23:05 -0500

    KMenuSha

--- modules/home/plasma/custom/kmenu.nix
+++ modules/home/plasma/custom/kmenu.nix
@@ -10,7 +10,7 @@ stdenv.mkDerivation {
     owner = "51n7";
     repo = "kMenu";
     rev = "HEAD";
-    sha256 = "sha256-Y7s9qdJIJbUqEP0/6qlTPOtE3efRqL1bx66MJIPgRN4=";
+    sha256 = "sha256-XKf3fBpx19J888XuufmmoDH7oz1H+fYCTW4yZ6Zjqvs=";
   };
 
   installPhase = ''
