1320de6ee269f040ed0b26f8c3d1a8badccfce0d
victor7w7r <arkano036@gmail.com>
2025-12-28 11:12:22 -0500

    ReuseLayan

--- modules/home/plasma/custom/layan.nix
+++ modules/home/plasma/custom/layan.nix
@@ -1,6 +1,5 @@
 {
   stdenv,
-  lib,
   fetchFromGitHub,
   gitUpdater,
 }:
@@ -11,7 +10,7 @@ stdenv.mkDerivation rec {
   src = fetchFromGitHub {
     owner = "vinceliuice";
     repo = pname;
-    rev = "7ab7cd7461dae8d8d6228d3919efbceea5f4272c";
+    rev = "2025-02-13";
     hash = "sha256-Wh8tZcQEdTTlgtBf4ovapojHcpPBZDDkWOclmxZv9zA=";
   };
 
@@ -22,29 +21,14 @@ stdenv.mkDerivation rec {
     substituteInPlace install.sh \
       --replace '$HOME/.local' $out \
       --replace '$HOME/.config' $out/share
-
-    if [ -d sddm ]; then
-      find sddm -name "*.qml" -exec sed -i "s|/usr|$out|g" {} +
-    fi
   '';
 
   installPhase = ''
     runHook preInstall
-    mkdir -p $out/share/sddm/themes
+    mkdir -p $out/share/sddm
     ./install.sh
-    if [ -d sddm ]; then
-        mkdir -p $out/share/sddm/themes
-        cp -ra sddm/Layan* $out/share/sddm/themes
-    fi
     runHook postInstall
   '';
 
   passthru.updateScript = gitUpdater { };
-
-  meta = with lib; {
-    description = "Flat Design theme for KDE Plasma desktop";
-    homepage = "https://github.com/vinceliuice/Layan-kde";
-    license = licenses.gpl3Only;
-    platforms = platforms.all;
-  };
 }
