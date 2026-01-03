84e0c81ca0e521e4e12de769639bca6525556aa2
victor7w7r <arkano036@gmail.com>
2026-01-02 21:17:58 -0500

    RemoveFlatpak

--- modules/core/desktop/plasma.nix
+++ modules/core/desktop/plasma.nix
@@ -82,25 +82,26 @@
     ulauncher
   ];
 
-  flatpak = {
-    enable = true;
-    /*
-      KDiskFree
-      OptiImage
-        packages = [
-          "io.github.DenysMb.Kontainer"
-          "io.github.nyre221.kiview"
-          "org.kde.kommit"
-          "com.github.d4nj1.tlpui"
-          "in.srev.guiscrcpy"
-          "com.github.vikdevelop.photopea_app"
-          "com.github.tchx84.Flatseal"
-          "io.emeric.toolblex"
-        ];
-        update.auto = {
-          enable = true;
-          onCalendar = "weekly";
-          };
-    */
-  };
+  /*
+    flatpak = {
+      enable = true;
+        KDiskFree
+        OptiImage
+          packages = [
+            "io.github.DenysMb.Kontainer"
+            "io.github.nyre221.kiview"
+            "org.kde.kommit"
+            "com.github.d4nj1.tlpui"
+            "in.srev.guiscrcpy"
+            "com.github.vikdevelop.photopea_app"
+            "com.github.tchx84.Flatseal"
+            "io.emeric.toolblex"
+          ];
+          update.auto = {
+            enable = true;
+            onCalendar = "weekly";
+            };
+
+            };
+  */
 }
