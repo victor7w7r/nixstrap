ad91c7f02b7164e6c5890c328a405579f2c8c8a9
victor7w7r <arkano036@gmail.com>
2026-01-02 21:36:00 -0500

    AddJetbrainsUnfree

--- modules/home/dev/packages.nix
+++ modules/home/dev/packages.nix
@@ -1,12 +1,17 @@
-{ pkgs, ... }:
+{ pkgs, lib, ... }:
 {
+  nixpkgs.config.allowUnfreePredicate =
+    pkg:
+    builtins.elem (lib.getName pkg) [
+      "jetbrains.datagrip"
+    ];
+
   home.packages = (
     with pkgs;
     [
       bruno
       cool-retro-term
       git-credential-manager
-      jetbrains.datagrip
       lazygit
       #notepadqq
       windterm
