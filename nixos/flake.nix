fde1346bdf3c15f2c91ea858d4904912881dd6c9
victor7w7r <arkano036@gmail.com>
2026-02-12 18:10:13 -0500

    AddFlakeFOrUpdate

--- nixos/flake.nix
+++ nixos/flake.nix
@@ -53,10 +53,7 @@
       url = "gitlab:rycee/nur-expressions?dir=pkgs/firefox-addons";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    zen-browser = {
-      url = "github:0xc000022070/zen-browser-flake";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+    zen-browser.url = "github:0xc000022070/zen-browser-flake";
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
