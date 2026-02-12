f8fe3cea5e75f79499597eccbf6fee839fe087cb
victor7w7r <arkano036@gmail.com>
2026-02-12 10:24:13 -0500

    RemoveGitWarning

--- nixos/modules/home/dev/config.nix
+++ nixos/modules/home/dev/config.nix
@@ -10,11 +10,13 @@
     git = {
       enable = true;
       lfs.enable = true;
-      userName = "victor7w7r";
-      userEmail = "arkano036@gmail.com";
       settings = {
         core.pager = "${pkgs.delta}/bin/delta";
         init.defaultBranch = "main";
+        user = {
+          name = "victor7w7r";
+          email = "arkano036@gmail.com";
+        };
         credential.helper = "store";
         delta = {
           hyperlinks = true;
