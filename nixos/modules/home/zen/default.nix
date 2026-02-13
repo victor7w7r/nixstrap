0de2eb942204443519486e85fbcfcc6d9d9a81b9
victor7w7r <arkano036@gmail.com>
2026-02-12 20:28:33 -0500

    ChangeToDefault

--- nixos/modules/home/zen/default.nix
+++ nixos/modules/home/zen/default.nix
@@ -37,7 +37,7 @@ let
     '';
   };
   zen-package =
-    (inputs.zen-browser.packages.${pkgs.stdenv.hostPlatform.system}.twilight.override {
+    (inputs.zen-browser.packages.${pkgs.stdenv.hostPlatform.system}.default.override {
       #policies = cfg_orig.policies;
     }).overrideAttrs
       (prev: {
