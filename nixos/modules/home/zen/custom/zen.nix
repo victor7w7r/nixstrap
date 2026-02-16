dfbcde6a8d29ec5efe615c0e9587d22370058164
victor7w7r <arkano036@gmail.com>
2026-02-16 18:45:10 -0500

    RemoveManualZen

--- nixos/modules/home/zen/custom/zen.nix
+++ nixos/modules/home/zen/custom/zen.nix
@@ -3,25 +3,29 @@
   system,
   inputs,
 }:
-let
-  sineUrl = "https://raw.githubusercontent.com/sineorg/bootloader/refs/heads/main/program";
-  libNameGlob = "zen-bin-*";
-  sineconfig = (
-    builtins.fetchurl {
-      url = "${sineUrl}/config.js";
-      sha256 = "117a6gkaz1kinjflfzqc6qsb4r06x93w08q4lfdzh5a1cng95s5v";
-    }
-  );
-  configpref = (
-    builtins.fetchurl {
-      url = "${sineUrl}/defaults/pref/config-prefs.js";
-      sha256 = "1kkyq5qdp7nnq09ckbd3xgdhsm2q80xjmihgiqbzb3yi778jxzbb";
-    }
-  );
-in
+/*
+  let
+    sineUrl = "https://raw.githubusercontent.com/sineorg/bootloader/refs/heads/main/program";
+    libNameGlob = "zen-bin-*";
+    sineconfig = (
+      builtins.fetchurl {
+        url = "${sineUrl}/config.js";
+        sha256 = "117a6gkaz1kinjflfzqc6qsb4r06x93w08q4lfdzh5a1cng95s5v";
+      }
+    );
+    configpref = (
+      builtins.fetchurl {
+        url = "${sineUrl}/defaults/pref/config-prefs.js";
+        sha256 = "1kkyq5qdp7nnq09ckbd3xgdhsm2q80xjmihgiqbzb3yi778jxzbb";
+      }
+    );
+  in
+*/
 (inputs.zen-browser.packages.${system}.twilight-unwrapped.override {
   inherit policies;
-}).overrideAttrs
+})
+/*
+  .overrideAttrs
   (prev: {
     postInstall = (prev.postInstall or "") + ''
       for libdir in "$out"/lib/${libNameGlob}; do
@@ -31,4 +35,5 @@ in
         cp "${configpref}" "$libdir/defaults/pref/config-pref.js"
       done
     '';
-  })
+    })
+*/
