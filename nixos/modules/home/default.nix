8bdf7dfe084f3654d5bcdfb0075e4414d12c4bf7
victor7w7r <arkano036@gmail.com>
2026-02-18 08:08:30 -0500

    SetDefault

--- nixos/modules/home/default.nix
+++ nixos/modules/home/default.nix
@@ -27,7 +27,7 @@
     useGlobalPkgs = true;
     sharedModules = [
       inputs.plasma-manager.homeModules.plasma-manager
-      inputs.zen-browser.homeModules.twilight-official
+      inputs.zen-browser.homeModules.default
       inputs.nix-doom-emacs-unstraightened.homeModule
     ];
 
