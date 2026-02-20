a8427d6ea9d39bd001f9dfdacd7abdd366a8dae9
victor7w7r <arkano036@gmail.com>
2026-02-19 21:30:08 -0500

    AddProxmoxOverlayDependency

--- nixos/modules/core/selfhost/proxmox.nix
+++ nixos/modules/core/selfhost/proxmox.nix
@@ -1,5 +1,21 @@
 { lib, ... }:
 {
+
+  nixpkgs.overlays = [
+    (self: super: {
+      perlPackages = super.perlPackages.override {
+        overrides = {
+          MIMEBase32 = super.perlPackages.MIMEBase32.overrideAttrs (oldAttrs: {
+            src = super.fetchurl {
+              url = "mirror://cpan/authors/id/R/RE/REHSACK/MIME-Base32-1.303.tar.gz";
+              hash = "sha256-sET89ToW1bg0UBNf33wNSwwr6ene383z0TPcC3khlzw=";
+            };
+          });
+        };
+      };
+    })
+  ];
+
   services.proxmox-ve = {
     enable = true;
     ipAddress = "192.168.1.100";
