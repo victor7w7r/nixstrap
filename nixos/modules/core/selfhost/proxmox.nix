aad4ba72b450c20851aa84be14295da0b8c0c0dd
victor7w7r <arkano036@gmail.com>
2026-02-19 21:36:05 -0500

    AddProxmoxOverlayDependencyWithSrv64

--- nixos/modules/core/selfhost/proxmox.nix
+++ nixos/modules/core/selfhost/proxmox.nix
@@ -4,13 +4,21 @@
   nixpkgs.overlays = [
     (self: super: {
       perlPackages = super.perlPackages // {
+
         MIMEBase32 = super.perlPackages.MIMEBase32.overrideAttrs (oldAttrs: {
           src = super.fetchurl {
             url = "mirror://cpan/authors/id/R/RE/REHSACK/MIME-Base32-1.303.tar.gz";
-            # El hash que tu sistema "vio" y que Nix nos exige
             hash = "sha256-sET89ToW1bg0UBNf33wNSwwr6ene383z0TPcC3khlzw=";
           };
         });
+
+        MIME-Base64 = super.perlPackages.MIME-Base64.overrideAttrs (oldAttrs: {
+          src = super.fetchurl {
+            url = "mirror://cpan/authors/id/C/CA/CAPOEIRAB/MIME-Base64-3.16.tar.gz";
+            hash = "sha256-9oNRf1bwLzALYZrcjWLbTS0LJ80svmCHSoAIQlDokVA=";
+          };
+        });
+
       };
     })
   ];
