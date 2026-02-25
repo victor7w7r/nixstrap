19f77917e496cf3e59e17bd50f1f1868431db662
victor7w7r <arkano036@gmail.com>
2026-02-24 21:50:55 -0500

    RemovePatchMacros

--- nixos/kernels/lib/patches.nix
+++ nixos/kernels/lib/patches.nix
@@ -38,7 +38,7 @@ let
     #"0002-platform-x86-asus-wmi-don-t-fail-if-platform_profile.patch"
     #"0003-asus-bios-refactor-existing-tunings-in-to-asus-bios-.patch"
     #"0003-HID-asus-add-ROG-Ally-N-Key-ID-and-keycodes.patch"
-    "0003-platform-x86-asus-wmi-add-macros-and-expose-min-max-.patch"
+    #"0003-platform-x86-asus-wmi-add-macros-and-expose-min-max-.patch"
     "0004-asus-bios-add-panel-hd-control.patch"
     "0005-asus-wmi-don-t-error-out-if-platform_profile-already.patch"
     "0006-asus-bios-add-apu-mem.patch"
