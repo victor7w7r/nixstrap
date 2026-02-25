2f7e4086ad7c3ae1993aa936af2d291c2e5370f3
victor7w7r <arkano036@gmail.com>
2026-02-24 20:51:43 -0500

    UpdateRogKernel

--- nixos/kernels/rog.nix
+++ nixos/kernels/rog.nix
@@ -23,7 +23,7 @@ let
     asus = true;
     src = fetchurl {
       url = "mirror://kernel/linux/kernel/v${lib.versions.major version}.x/linux-${version}.tar.xz";
-      hash = "sha256-bQiAO5U8UJ30jUTTKB7TklJDIdi7NT6yHAVVeQyPjgY=";
+      hash = "sha256-DkdJaK38vuMpFv0BqJ2Mz9EWjY0yVp52pcZkx5MZjr4=";
     };
   };
 in
