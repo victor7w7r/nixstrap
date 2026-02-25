6f79528e84fa53c277d1629c87ba3cd2e842fcad
victor7w7r <arkano036@gmail.com>
2026-02-24 21:48:50 -0500

    RemovePatch

--- nixos/kernels/lib/patches.nix
+++ nixos/kernels/lib/patches.nix
@@ -36,7 +36,7 @@ let
     #"0001-HID-asus-fix-more-n-key-report-descriptors-if-n-key-.patch"
     #"0002-HID-asus-make-asus_kbd_init-generic-remove-rog_nkey_.patch"
     #"0002-platform-x86-asus-wmi-don-t-fail-if-platform_profile.patch"
-    "0003-asus-bios-refactor-existing-tunings-in-to-asus-bios-.patch"
+    #"0003-asus-bios-refactor-existing-tunings-in-to-asus-bios-.patch"
     #"0003-HID-asus-add-ROG-Ally-N-Key-ID-and-keycodes.patch"
     "0003-platform-x86-asus-wmi-add-macros-and-expose-min-max-.patch"
     "0004-asus-bios-add-panel-hd-control.patch"
