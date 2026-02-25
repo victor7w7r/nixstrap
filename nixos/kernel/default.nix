bdd6f46a32ae3dd89cc5859eeb894810cce89301
victor7w7r <arkano036@gmail.com>
2026-02-25 18:43:09 -0500

    RemoveHostInherit

--- nixos/kernel/default.nix
+++ nixos/kernel/default.nix
@@ -11,7 +11,7 @@ let
     (import ./config.nix) { inherit host lib; } // (pkgs.callPackage ./simplify.nix { }).general;
   helpers = (pkgs.callPackage ./helpers.nix { });
   kernelConfig = (pkgs.callPackage ./kernel-config.nix { inherit hardened; });
-  source = (pkgs.callPackage ./source.nix { inherit hardened host; });
+  source = (pkgs.callPackage ./source.nix { inherit hardened; });
 
   nativeHost =
     if host == "v7w7r-macmini81" then
