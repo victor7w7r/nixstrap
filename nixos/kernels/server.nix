7eb4ce6958d168db18d48ab376f550e64ebb4e2f
victor7w7r <arkano036@gmail.com>
2026-02-25 13:47:26 -0500

    SetKernelVersionLTSOlder5ShaTypo

--- nixos/kernels/server.nix
+++ nixos/kernels/server.nix
@@ -19,7 +19,7 @@ let
     argsOverride = rec {
       src = pkgs.fetchurl {
         url = "mirror://kernel/linux/kernel/v6.x/linux-${version}.tar.xz";
-        sha256 = " sha256-xDW9dNHCH8WpUHgaUNeLriuTlEFEaUhDNZlIrTr8cqU=";
+        sha256 = "sha256-xDW9dNHCH8WpUHgaUNeLriuTlEFEaUhDNZlIrTr8cqU=";
       };
       version = "6.12.50";
       modDirVersion = "6.12.50";
