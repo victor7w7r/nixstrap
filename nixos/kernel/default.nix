e6de602c2666844a4077b60dac9289e7c0138c7b
victor7w7r <arkano036@gmail.com>
2026-03-01 09:57:23 -0500

    ADDConfigKernelFile

--- nixos/kernel/default.nix
+++ nixos/kernel/default.nix
@@ -12,7 +12,7 @@ let
   kernel =
     (pkgs.linuxManualConfig {
       src = source.src;
-      config = "${source}/.config";
+      configFile = "${source}/.config";
       allowImportFromDerivation = true;
       version = source.version;
       modDirVersion = lib.versions.pad 3 "${source.version}${source.passthru.localVer}";
