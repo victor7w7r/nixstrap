f630199c09a228865b55ca2c0a67951299ada2ba
victor7w7r <arkano036@gmail.com>
2026-03-01 12:02:02 -0500

    RemoveDiff

--- nixos/kernel/source.nix
+++ nixos/kernel/source.nix
@@ -105,7 +105,7 @@ pkgs.stdenv.mkDerivation {
 
     make olddefconfig
     patchShebangs scripts/config
-    diff .config.old .config
+    #diff .config.old .config
     ./scripts/config --file .config ${lib.concatStringsSep " " config}
     make olddefconfig
     grep "CONFIG_LTO_CLANG_THIN" .config || echo "¡OJO! LTO no se activó"
