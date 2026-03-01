332e83d345eecb51b59bfd4f5227d7a1bb525706
victor7w7r <arkano036@gmail.com>
2026-03-01 11:50:31 -0500

    FixLto

--- nixos/kernel/source.nix
+++ nixos/kernel/source.nix
@@ -104,7 +104,7 @@ pkgs.stdenv.mkDerivation {
 
     make olddefconfig
     patchShebangs scripts/config
-    .scripts/config --file .config ${lib.concatStringsSep " " config}
+    ./scripts/config --file .config ${lib.concatStringsSep " " config}
     make olddefconfig
     grep "CONFIG_LTO_CLANG_THIN" .config || echo "¡OJO! LTO no se activó"
   '';
