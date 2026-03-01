cad814c5601ddbbd76d9afe54ad4064faa08404e
victor7w7r <arkano036@gmail.com>
2026-03-01 15:31:20 -0500

    AddRandStructu

--- nixos/kernel/source.nix
+++ nixos/kernel/source.nix
@@ -39,40 +39,42 @@ let
 
   fetch = import ./fetch.nix {
     inherit hardened pkgs;
-    kernelHash = "sha256-fHFiFsPEE07Q3mkZVwHmd1d7vN05efMxwYKs0Gvy8XA=";
     asusPatchesHash = "sha256-3G/oLfYdL+g+OoacjOuEwFg7/EyLPxKCnlZfHOYWmTk=";
     asusPatchesRev = "0e4aca508d46305a4d3fdf814c5d2bded30a2cdb";
     kernelConfigHash = "sha256-d1GhWEdENpt002r7mmVJ6n4FqJ/W+m8IZJl5ioWDwjo=";
     cachyPatchesHash = "sha256-LhKeRpbG355d/h0H+esisnZ695I7PTFjEkOHKeEtO54=";
   };
 
-  patches = [
-    kernelPatches.bridge_stp_helper.patch
-    kernelPatches.request_key_helper.patch
-    "${fetch.cachy-patches}/${majorMinor}/all/0001-cachyos-base-all.patch"
-  ]
-  ++ (lib.optional (host != "v7w7r-youyeetoox1") [
-    "${fetch.cachy-patches}/${majorMinor}/sched/0001-bore-cachy.patch"
-  ])
-  ++ (lib.optional hardened [
-    "${fetch.cachy-patches}/${majorMinor}/misc/0001-hardened.patch"
-  ])
-  ++ (lib.optional (host == "v7w7r-rc71l") (
-    [
-      "${fetch.cachy-patches}/${majorMinor}/misc/0001-acpi-call.patch"
-      "${fetch.cachy-patches}/${majorMinor}/misc/0001-handheld.patch"
+  patches =
+    let
+      rmRandstruct = with lib; filter (p: !hasInfix "randstruct" p);
+    in
+    (rmRandstruct baseKernel.patches)
+    ++ [
+      "${fetch.cachy-patches}/${majorMinor}/all/0001-cachyos-base-all.patch"
     ]
-    ++ builtins.map (p: "${fetch.asus-patches.outPath}/${p}") [
-      "0001-bluetooth-btus-add-new-vid-pid.patch"
-      "0002-platform-x86-asus-armoury-add-keyboard-control-firmw.patch"
-      "0040-workaround_hardware_decoding_amdgpu.patch"
-      "0070-acpi-x86-s2idle-Add-ability-to-configure-wakeup-by-A.patch"
-      "0081-amdgpu-adjust_plane_init_off_by_one.patch"
-      "asus-patch-series.patch"
-      "PATCH-v5-00-11-Improvements-to-S5-power-consumption.patch"
-      "v2-0002-hid-asus-change-the-report_id-used-for-HID-LED-co.patch"
-    ]
-  ));
+    ++ (lib.optional (host != "v7w7r-youyeetoox1") [
+      "${fetch.cachy-patches}/${majorMinor}/sched/0001-bore-cachy.patch"
+    ])
+    ++ (lib.optional hardened [
+      "${fetch.cachy-patches}/${majorMinor}/misc/0001-hardened.patch"
+    ])
+    ++ (lib.optional (host == "v7w7r-rc71l") (
+      [
+        "${fetch.cachy-patches}/${majorMinor}/misc/0001-acpi-call.patch"
+        "${fetch.cachy-patches}/${majorMinor}/misc/0001-handheld.patch"
+      ]
+      ++ builtins.map (p: "${fetch.asus-patches.outPath}/${p}") [
+        "0001-bluetooth-btus-add-new-vid-pid.patch"
+        "0002-platform-x86-asus-armoury-add-keyboard-control-firmw.patch"
+        "0040-workaround_hardware_decoding_amdgpu.patch"
+        "0070-acpi-x86-s2idle-Add-ability-to-configure-wakeup-by-A.patch"
+        "0081-amdgpu-adjust_plane_init_off_by_one.patch"
+        "asus-patch-series.patch"
+        "PATCH-v5-00-11-Improvements-to-S5-power-consumption.patch"
+        "v2-0002-hid-asus-change-the-report_id-used-for-HID-LED-co.patch"
+      ]
+    ));
 in
 pkgs.stdenv.mkDerivation {
   #inherit patches;
