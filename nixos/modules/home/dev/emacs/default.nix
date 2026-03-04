7ffc3520469d86bdd177a4022d14a733bde32417
victor7w7r <arkano036@gmail.com>
2026-03-04 12:57:43 -0500

    RemoveAugmenty

--- nixos/modules/home/dev/emacs/default.nix
+++ nixos/modules/home/dev/emacs/default.nix
@@ -17,17 +17,6 @@
     extraPackages =
       epkgs: with epkgs; [
         melpaPackages.nixos-options
-        (epkgs.melpaBuild {
-          pname = "lsp-augment";
-          version = "v0.33.0";
-          packageRequires = [ epkgs.dash ];
-          src = builtins.fetchTree {
-            type = "github";
-            rev = "06819e1158e75a3c264cf6776ce0b9bf2f69f241";
-            owner = "rolandd";
-            repo = "augment.vim";
-          };
-        })
       ];
     extraBinPackages = with pkgs; [
       git
