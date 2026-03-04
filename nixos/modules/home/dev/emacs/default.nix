741be5c6644da724f398e19cec28b7dcc9009432
victor7w7r <arkano036@gmail.com>
2026-03-04 10:34:12 -0500

    NixDoomAddLocal

--- nixos/modules/home/dev/emacs/default.nix
+++ nixos/modules/home/dev/emacs/default.nix
@@ -1,13 +1,14 @@
-{ pkgs, ... }:
+{ config, pkgs, ... }:
 {
   programs.doom-emacs = {
     enable = true;
     emacs = pkgs.emacs-nox;
     doomDir = ./.;
-    doomLocalDir = "~/.local/share/nix-doom";
-    extraPackages = epkgs: with epkgs; [
-      melpaPackages.nixos-options
-    ];
+    doomLocalDir = "${config.home.homeDirectory}/.local/share/nix-doom";
+    extraPackages =
+      epkgs: with epkgs; [
+        melpaPackages.nixos-options
+      ];
     extraBinPackages = with pkgs; [
       git
       ripgrep
