8af41cf67ad682c1b0a26db9767bbd5af5ead930
victor7w7r <arkano036@gmail.com>
2026-03-12 16:41:39 -0500

    FixConfigVersion

--- nixos/kernel/default.nix
+++ nixos/kernel/default.nix
@@ -36,8 +36,8 @@ let
       config = linuxConfigTransfomed;
       configfile = configure;
       allowImportFromDerivation = false;
-      version = configure.version;
-      modDirVersion = lib.versions.pad 3 "${configure.version}${configure.passthru.localVer}";
+      version = lib.versions.pad 3 "${configure.version}${configure.passthru.localVer}";
+      modDirVersion = configure.version;
       stdenv = helpers.stdenvLLVM;
 
       kernelPatches = builtins.map (file: {
@@ -47,6 +47,7 @@ let
 
       extraMakeFlags = [
         "NIX_ENFORCE_NO_NATIVE=0"
+        "LOCALVERSION=${configure.passthru.localVer}"
         "NIX_CC_WRAPPER_SUPPRESS_TARGET_WARNING=1"
         "KCFLAGS=-Wno-error"
       ];
