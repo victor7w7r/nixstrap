704370bbab9e1a6f2d16eabdc5158a2f047f256d
victor7w7r <arkano036@gmail.com>
2026-03-15 11:24:21 -0500

    FixImportOrder

--- nixos/kernel/config/default.nix
+++ nixos/kernel/config/default.nix
@@ -5,18 +5,19 @@
 ++ (import ./vendors.nix)
 ++ (import ./trace.nix)
 ++ (
-  if host == "v7w7r-youyeetoox1" then
-    (import ./server.nix)
-  else
-    (import ./general.nix)
-) ++ (
   if host == "v7w7r-rc71l" then
     (import ./asus-amd.nix)
   else
     (import ./intel.nix)
-) ++ (
-  if host == "v7w7r-higole" then
-    (import ./higole.nix)
-  else
-    (import ./responsive.nix)
+)
+++ (
+  if host == "v7w7r-youyeetoox1" then
+    (import ./server.nix)
+  else ((
+      if host == "v7w7r-higole" then
+        (import ./higole.nix)
+      else
+        (import ./responsive.nix)
+    ) ++ (import ./general.nix)
+  )
 )
