0a50112f08a3ac2e5fc136e45f10d7690965c576
victor7w7r <arkano036@gmail.com>
2026-03-18 11:25:20 -0500

    RemoveAnaly

--- nixos/kernel/configure.nix
+++ nixos/kernel/configure.nix
@@ -49,7 +49,7 @@ let
     #"${fetch.tachyon}/patches/0001-mm-memcontrol-add-some-branch-hints-based-on-gcov-an.patch"
     #"${fetch.tachyon}/patches/0002-kernel-Eliminate-dynamic-memory-allocation-in-prctl_.patch"
     #"${fetch.tachyon}/patches/0002-mm-disable-proactive-compaction-by-de.patch"
-    "${fetch.tachyon}/patches/0002-sched-core-add-some-branch-hints-based-on-gcov-analy.patch"
+    #"${fetch.tachyon}/patches/0002-sched-core-add-some-branch-hints-based-on-gcov-analy.patch"
     "${fetch.tachyon}/patches/0002-sched-Disable-TTWU_QUEUE.patch"
     "${fetch.tachyon}/patches/0003-mm-Omit-RCU-read-lock-in-list_lru_count_one-when-RCU.patch"
     "${fetch.tachyon}/patches/0003-mm-stop-kswapd-early-when-nothings-wa.patch"
