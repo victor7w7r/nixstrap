540a43e578fef7a102a0e0a7039503726c3453e1
victor7w7r <arkano036@gmail.com>
2026-03-19 11:51:37 -0500

    RemoveClangPolly

--- nixos/kernel/configure.nix
+++ nixos/kernel/configure.nix
@@ -30,11 +30,11 @@ let
   localVer = "-v7w7r${specialization}${if hardened then "-hardened" else ""}-native";
 
   patches = [
+    "${fetch.patches}/${majorMinor}/0003-bbr3.patch"
+    "${fetch.patches}/${majorMinor}/0004-cachy.patch"
+    "${fetch.patches}/${majorMinor}/0005-crypto.patch"
+    "${fetch.patches}/${majorMinor}/0006-fixes.patch"
     /*
-      "${fetch.patches}/${majorMinor}/0003-bbr3.patch"
-      "${fetch.patches}/${majorMinor}/0004-cachy.patch"
-      "${fetch.patches}/${majorMinor}/0005-crypto.patch"
-      "${fetch.patches}/${majorMinor}/0006-fixes.patch"
       "${fetch.patches}/${majorMinor}/misc/0001-clang-polly.patch"
       "${fetch.patches}/${majorMinor}/misc/dkms-clang.patch"
       "${fetch.patches}/${majorMinor}/misc/poc-selector.patch"
