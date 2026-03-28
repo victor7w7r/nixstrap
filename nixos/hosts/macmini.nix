05f47b906d54b89184a800ab6a68ca02b1615f58
TCMP-CJimenez <arkano036@gmail.com>
2026-03-28 08:58:05 -0500

    PutForceBrackets

--- nixos/hosts/macmini.nix
+++ nixos/hosts/macmini.nix
@@ -64,12 +64,12 @@ in
   ];
   boot = {
     kernelParams = [ "intel_iommu=on" ] ++ intelParams ++ params { };
-    kernelPackages = (lib.mkForce helpers.kernelModuleLLVMOverride (kernelBuild.packages)).extend (
+    kernelPackages = ( lib.mkForce (helpers.kernelModuleLLVMOverride (kernelBuild.packages)).extend (
       _self: _super: {
         kernel_configfile = _super.kernel.configfile;
         zfs_cachyos = pkgs.cachyosKernels.zfs-cachyos-lto.override { kernel = kernelBuild.kernel; };
       }
-    );
+    ));
 
     zfs = {
       package = config.boot.kernelPackages.zfs_cachyos;
