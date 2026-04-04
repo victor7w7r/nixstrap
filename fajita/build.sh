f07277793fc0552178716f2ac678cd8149cb9e8a
victor7w7r <arkano036@gmail.com>
2026-04-04 18:38:54 -0500

    FixArmEmu

--- fajita/build.sh
+++ fajita/build.sh
@@ -2,3 +2,6 @@
 
 podman run --privileged --rm docker.io/tonistiigi/binfmt --install all
 podman run -it --platform linux/arm64 docker.io/nixos/nix:latest-arm64
+mkdir -p dist
+podman compose up --build
+podman compose down
