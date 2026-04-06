cc6c47632b9633a7df7655f88dad69d8ffd101bd
victor7w7r <arkano036@gmail.com>
2026-04-06 17:23:19 -0500

    AddKernelImports

--- nixos/modules/core/boot/default.nix
+++ nixos/modules/core/boot/default.nix
@@ -2,13 +2,14 @@
 {
   imports = [
     (import ./emulation.nix)
+    (import ./kernel.nix)
     (import ./sysctl.nix)
+    (import ./persist.nix)
   ]
   ++ (
     if (host != "v7w7r-opizero2w") then
       [
         (import ./bootloader.nix)
-        (import ./persist.nix)
       ]
     else
       [ ]
