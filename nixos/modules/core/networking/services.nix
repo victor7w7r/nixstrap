1f46b2135acbf45d980ef3e87ede454fb2e857c0
victor7w7r <arkano036@gmail.com>
2026-04-10 20:10:31 -0500

    fix: fajita host in separated set

--- nixos/modules/core/networking/services.nix
+++ nixos/modules/core/networking/services.nix
@@ -5,25 +5,26 @@
     cockpit.enable = true;
     croc.enable = true;
     dnsmasq.enable = true;
-    openssh = {
-      enable = true;
-    }
-    // (
-      if host == "v7w7r-fajita" then
-        {
-          settings = {
-            PermitRootLogin = "yes";
-            PasswordAuthentication = true;
-          };
-        }
-      else
-        { }
-    );
+
     tailscale.enable = true;
     ttyd = {
       enable = true;
       writeable = true;
     };
     #openvpn.package = true;
-  };
+  }
+  // (
+    if host == "v7w7r-fajita" then
+      {
+        openssh = {
+          enable = true;
+          settings = {
+            PermitRootLogin = "yes";
+            PasswordAuthentication = true;
+          };
+        };
+      }
+    else
+      { openssh.enable = true; }
+  );
 }
