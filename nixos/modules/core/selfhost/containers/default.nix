71d7a3f44caf1fa9f2f2dc462745dbee6a9f0efa
victor7w7r <arkano036@gmail.com>
2026-04-12 11:39:59 -0500

    feat: add notes for cloud

---
+++ nixos/modules/core/selfhost/containers/default.nix
@@ -0,0 +1,6 @@
+{ ... }:
+{
+  imports = [
+    ./notes.nix
+  ];
+}
--- nixos/modules/core/selfhost/default.nix
+++ nixos/modules/core/selfhost/default.nix
@@ -1,8 +1,9 @@
 { ... }:
 {
   imports = [
-    (import ./harmonia.nix)
-    (import ./proxmox.nix)
-    (import ./sleep.nix)
+    ./containers
+    ./harmonia.nix
+    ./proxmox.nix
+    ./sleep.nix
   ];
 }
