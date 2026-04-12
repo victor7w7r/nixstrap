f5d5ae35162a1231e90c448e3c658ef98d2010cc
victor7w7r <arkano036@gmail.com>
2026-04-12 17:19:47 -0500

    fix: add couchdb extra config in set

--- nixos/modules/core/selfhost/containers/notes.nix
+++ nixos/modules/core/selfhost/containers/notes.nix
@@ -53,33 +53,39 @@
         services.couchdb = {
           enable = true;
           bindAddress = "0.0.0.0";
-          extraConfig = ''
-            [couchdb]
-            single_node=true
-            max_http_request_size = 4294967296
-            max_document_size = 50000000
+          extraConfig = {
+            couchdb = {
+              single_node = true;
+              max_http_request_size = 4294967296;
+              max_document_size = 50000000;
+            };
 
-            [chttpd]
-            require_valid_user = true
-            max_http_request_size = 4294967296
-            enable_cors = true
+            chttpd = {
+              require_valid_user = true;
+              max_http_request_size = 4294967296;
+              enable_cors = true;
+            };
 
-            [chttpd_auth]
-            require_valid_user = true
-            authentication_redirect = /_utils/session.html
+            chttpd_auth = {
+              require_valid_user = true;
+              authentication_redirect = "/_utils/session.html";
+            };
 
-            [httpd]
-            WWW-Authenticate = Basic realm="couchdb"
-            enable_cors = true
-            bind_address = 0.0.0.0
+            httpd = {
+              WWW-Authenticate = ''Basic realm="couchdb"'';
+              enable_cors = true;
+              bind_address = "0.0.0.0";
+            };
 
-            [cors]
-            origins = app://obsidian.md, capacitor://localhost, http://localhost
-            credentials = true
-            headers = accept, authorization, content-type, origin, referer
-            methods = GET,PUT,POST,HEAD,DELETE
-            max_age = 3600
-          '';
+            cors = {
+              origins = "app://obsidian.md, capacitor://localhost, http://localhost";
+              credentials = true;
+              headers = "accept, authorization, content-type, origin, referer";
+              methods = "GET,PUT,POST,HEAD,DELETE";
+              max_age = 3600;
+            };
+
+          };
           extraConfigFiles = [ "/run/secrets/couchdb-admins.ini" ];
         };
 
