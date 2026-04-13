88c49ff7dd54f35c44b269d3ccfaa1b0b2b2c52c
victor7w7r <arkano036@gmail.com>
2026-04-12 20:08:01 -0500

    fix: attempt for dns

--- nixos/modules/core/selfhost/containers/notes.nix
+++ nixos/modules/core/selfhost/containers/notes.nix
@@ -5,6 +5,10 @@
     privateNetwork = true;
     hostBridge = "br0";
     localAddress = "192.168.1.120/24";
+    nameservers = [
+      "192.168.1.1"
+      "8.8.8.8"
+    ];
     forwardPorts = [
       {
         containerPort = 5984;
@@ -52,53 +56,57 @@
         system.stateVersion = "26.05";
         boot.isContainer = true;
         networking = {
-          defaultGateway = "192.168.1.1";
-          nameservers = [
-            "8.8.8.8"
-            "1.1.1.1"
-          ];
           useHostResolvConf = lib.mkForce false;
-          nftables.enable = false;
-          firewall.enable = false;
-        };
-        services.resolved.enable = true;
-        services.couchdb = {
-          enable = true;
-          bindAddress = "0.0.0.0";
-          extraConfig = {
-            couchdb = {
-              single_node = true;
-              max_http_request_size = 4294967296;
-              max_document_size = 50000000;
-            };
-
-            chttpd = {
-              require_valid_user = true;
-              max_http_request_size = 4294967296;
-              enable_cors = true;
-            };
-
-            chttpd_auth = {
-              require_valid_user = true;
-              authentication_redirect = "/_utils/session.html";
-            };
-
-            httpd = {
-              WWW-Authenticate = ''Basic realm="couchdb"'';
-              enable_cors = true;
-              bind_address = "0.0.0.0";
-            };
-
-            cors = {
-              origins = "app://obsidian.md, capacitor://localhost, http://localhost";
-              credentials = true;
-              headers = "accept, authorization, content-type, origin, referer";
-              methods = "GET,PUT,POST,HEAD,DELETE";
-              max_age = 3600;
-            };
-
+          firewall = {
+            enable = true;
+            allowedTCPPorts = [
+              5984
+              8080
+              8443
+            ];
           };
-          extraConfigFiles = [ "/run/secrets/couchdb-admins.ini" ];
+        };
+
+        services = {
+          resolved.enable = true;
+          couchdb = {
+            enable = true;
+            bindAddress = "0.0.0.0";
+            extraConfig = {
+              couchdb = {
+                single_node = true;
+                max_http_request_size = 4294967296;
+                max_document_size = 50000000;
+              };
+
+              chttpd = {
+                require_valid_user = true;
+                max_http_request_size = 4294967296;
+                enable_cors = true;
+              };
+
+              chttpd_auth = {
+                require_valid_user = true;
+                authentication_redirect = "/_utils/session.html";
+              };
+
+              httpd = {
+                WWW-Authenticate = ''Basic realm="couchdb"'';
+                enable_cors = true;
+                bind_address = "0.0.0.0";
+              };
+
+              cors = {
+                origins = "app://obsidian.md, capacitor://localhost, http://localhost";
+                credentials = true;
+                headers = "accept, authorization, content-type, origin, referer";
+                methods = "GET,PUT,POST,HEAD,DELETE";
+                max_age = 3600;
+              };
+
+            };
+            extraConfigFiles = [ "/run/secrets/couchdb-admins.ini" ];
+          };
         };
 
         environment.systemPackages = with pkgs; [
@@ -106,23 +114,13 @@
           gnugrep
         ];
 
-        systemd = {
-          tmpfiles.rules = [
-            "d /opt/couchdb/data 0770 couchdb couchdb - -"
-            "d /opt/couchdb/etc/local.d 0770 couchdb couchdb - -"
-            "d /run/secrets/couchdb-admins.ini 0770 couchdb couchdb - -"
-            "d /web/vaults 0770 couchdb couchdb - -"
-            "d /web/config 0770 couchdb couchdb - -"
-          ];
-          services.couchdb-healthcheck = {
-            description = "CouchDB Healthcheck";
-            startAt = "*:*:0/30";
-            script = ''
-              ${pkgs.curl}/bin/curl --fail -s -u admin:password http://localhost:5984/_up \
-              | ${pkgs.gnugrep}/bin/grep -Eo '"status":"ok"' || exit 1
-            '';
-          };
-        };
+        systemd.tmpfiles.rules = [
+          "d /opt/couchdb/data 0770 couchdb couchdb - -"
+          "d /opt/couchdb/etc/local.d 0770 couchdb couchdb - -"
+          "d /run/secrets/couchdb-admins.ini 0770 couchdb couchdb - -"
+          "d /web/vaults 0770 couchdb couchdb - -"
+          "d /web/config 0770 couchdb couchdb - -"
+        ];
 
         virtualisation = {
           docker.enable = true;
