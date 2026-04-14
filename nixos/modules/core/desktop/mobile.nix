9e856acca781bd69d8bc1d4288fc1b6cec03aab2
victor7w7r <arkano036@gmail.com>
2026-04-14 15:00:00 -0500

    fix: plasma mobile session

--- nixos/modules/core/desktop/mobile.nix
+++ nixos/modules/core/desktop/mobile.nix
@@ -2,18 +2,22 @@
 {
   services = {
     libinput.enable = true;
-    displayManager = {
-      autoLogin.enable = true;
-      defaultSession = "plasma-mobile";
-    };
     xserver = {
       enable = true;
       desktopManager.plasma5.mobile.enable = true;
-      displayManager.lightdm = {
-        enable = true;
-        extraSeatDefaults = ''
-          session-cleanup-script=${pkgs.procps}/bin/pkill -P1 -fx ${pkgs.lightdm}/sbin/lightdm
-        '';
+      displayManager = {
+        autoLogin = {
+          enable = true;
+          user = "victor7w7wr";
+        };
+        defaultSession = "plasma-mobile";
+        sessionPackages = [ pkgs.kdePackages.plasma-mobile ];
+        lightdm = {
+          enable = true;
+          extraSeatDefaults = ''
+            session-cleanup-script=${pkgs.procps}/bin/pkill -P1 -fx ${pkgs.lightdm}/sbin/lightdm
+          '';
+        };
       };
     };
   };
