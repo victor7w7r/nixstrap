cf1fb1315ab47f7c095aed6f3c5fe4708803755e
victor7w7r <arkano036@gmail.com>
2026-04-13 22:06:25 -0500

    fix: add only one kernel

--- nixos/hosts/opizero2w.nix
+++ nixos/hosts/opizero2w.nix
@@ -1,7 +1,5 @@
 {
   lib,
-  host,
-  config,
   pkgs,
   kernelData,
   ...
@@ -12,24 +10,38 @@ let
     extraMeta.platforms = [ "aarch64-linux" ];
     BL31 = "${pkgs.armTrustedFirmwareAllwinnerH616}/bl31.bin";
     filesToInstall = [ "u-boot-sunxi-with-spl.bin" ];
+    version = "2024.04";
+    src = pkgs.fetchurl {
+      url = "https://ftp.denx.de/pub/u-boot/u-boot-2024.04.tar.bz2";
+      hash = "sha256-GKhT/jn6160DqQzC1Cda6u1tppc13vrDSSuAUIhD3Uo=";
+    };
+
+    nativeBuildInputs = with pkgs; [
+      dtc
+      armTrustedFirmwareTools
+      bison
+      flex
+      which
+      swig
+      openssl
+
+      (python3.withPackages (p: [
+        p.setuptools
+        p.libfdt
+        p.pyelftools
+      ]))
+    ];
+
   };
   kernel = (pkgs.callPackage ../kernel/sunxi) { inherit kernelData; };
 in
 {
-
-  imports = [
-    (import ./lib/sdcard.nix {
-      inherit config pkgs host;
-      rootVolumeLabel = "OPIZERO2W";
-      populateFirmwareCommands = ''
-        mkdir -p firmware/boot
-        ${config.boot.loader.generic-extlinux-compatible.populateCmd} \
-          -c ${config.system.build.toplevel} \
-          -d firmware/boot
-      '';
-      postBuildCommands = "dd if=${uboot}/u-boot-sunxi-with-spl.bin of=$img bs=1024 seek=8 conv=notrunc";
-    })
-  ];
+  sdImage = {
+    compressImage = true;
+    postBuildCommands = ''
+      dd if=${uboot}/u-boot-sunxi-with-spl.bin of=$img bs=1024 seek=8 conv=notrunc
+    '';
+  };
 
   nixpkgs.overlays = [
     (_final: super: {
@@ -43,30 +55,103 @@ in
     cacheDir = "/nix/var/cache/ccache-kernel";
   };
 
+  fileSystems."/persist".neededForBoot = true;
+
+  disko.devices = {
+    nodev."/" = {
+      fsType = "tmpfs";
+      mountOptions = [
+        "relatime"
+        "mode=755"
+        "size=1G"
+      ];
+    };
+
+    disk = {
+      sd = {
+        type = "disk";
+        device = "/dev/mmcblk0";
+        content = {
+          type = "gpt";
+          partitions = {
+            firmware = {
+              size = "1024M";
+              label = "FIRMWARE";
+              type = "0700";
+              content = {
+                type = "filesystem";
+                format = "vfat";
+                mountpoint = "/boot/firmware";
+                mountOptions = [
+                  "noatime"
+                  "noauto"
+                  "x-systemd.automount"
+                  "x-systemd.idle-timeout=1min"
+                ];
+              };
+            };
+
+            boot = {
+              size = "512M";
+              label = "BOOT";
+              type = "0700";
+              content = {
+                type = "filesystem";
+                format = "vfat";
+                mountpoint = "/boot";
+                mountOptions = [
+                  "noatime"
+                  "noauto"
+                  "x-systemd.automount"
+                  "x-systemd.idle-timeout=1min"
+                ];
+              };
+            };
+
+            nix = {
+              label = "NIX";
+              size = "100%";
+              content = {
+                type = "filesystem";
+                mountpoint = "/nix";
+                format = "f2fs";
+                mountOptions = [
+                  "lazytime"
+                  "noatime"
+                  "compress_chksum"
+                  "compress_algorithm=zstd:3"
+                  "age_extent_cache"
+                  "compress_extension=so"
+                  "inline_xattr"
+                  "inline_data"
+                  "inline_dentry"
+                  "errors=remount-ro"
+                  "compress_extension=bin"
+                  "atgc"
+                  "flush_merge"
+                  "discard"
+                  "checkpoint_merge"
+                  "gc_merge"
+                ];
+              };
+            };
+          };
+        };
+      };
+    };
+  };
+
   boot = {
-    kernelParams = lib.mkDefault [
-      "console=tty1"
-      "video=HDMI-A-1:1080p60"
-      "earlycon=uart8250,mmio32,0x05000000"
+    kernelParams = [
+      "earlycon"
+      "console=ttyS0,115200n8"
     ];
     loader.grub.enable = false;
     loader.generic-extlinux-compatible.enable = true;
     extraModprobeConfig = "options zram num_devices=1";
-    kernelPackages = kernel.packages;
-    kernelModules = [
-      "sprdwl_ng"
-      "sprdbt_tty"
-      "sun4i_drm"
-      "sun8i_mixer"
-      "hdmi"
-      "dw_hdmi"
-      "display_connector"
-      "sunxi_addr"
-      "zram"
-      "zstd"
-      "zstd_compress"
-      "crypto_zstd"
-    ];
+    #kernelPackages = kernel.packages;
+    kernelPackages = lib.mkDefault pkgs.linuxPackages_6_18;
+    initrd.availableKernelModules = [ "sunxi-mmc" ];
   };
 
   networking.wireless.enable = true;
@@ -81,12 +166,8 @@ in
     priority = 100;
   };
 
-  hardware = {
-    enableAllHardware = true;
-    firmware = [ (pkgs.callPackage ./custom/sunxi.nix { }) ];
-    deviceTree = {
-      enable = true;
-      name = "allwinner/sun50i-h618-orangepi-zero2w.dtb";
-    };
+  hardware.deviceTree = {
+    enable = true;
+    name = "allwinner/sun50i-h618-orangepi-zero2w.dtb";
   };
 }
