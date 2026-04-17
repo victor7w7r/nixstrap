69b7c1509d0db01859e1f4f83a6e507dd8070161
victor7w7r <arkano036@gmail.com>
2026-04-16 20:48:34 -0500

    fix: add config for kernel release in prepare

--- nixos/kernel/configure.nix
+++ nixos/kernel/configure.nix
@@ -143,6 +143,8 @@ pkgs.stdenv.mkDerivation (attrs: {
     ${(import ./cache.nix) { }}
     cp "${fetch.kConfig}" ".config"
 
+    make $makeFlags olddefconfig
+    make $makeFlags prepare
     mkdir -p include/config
     make $makeFlags kernelrelease > include/config/kernel.release
     ${((import ./modules) { inherit host; })}
