96ae1af8d3b5f7e242aa0e732e89374c6ded711a
victor7w7r <arkano036@gmail.com>
2026-04-16 20:36:53 -0500

    fix: add ccache in dat

--- nixos/kernel/cache.nix
+++ nixos/kernel/cache.nix
@@ -1,29 +1,11 @@
 {
   isClang ? true,
 }:
-
-let
-  wrapperBody = ''
-    for arg in "$@"; do
-      case "$arg" in
-        "-") exec "$real_compiler" "$@" ;;
-      esac
-    done
-    exec ccache "$real_compiler" "$@"
-  '';
-in
 ''
   export CCACHE_COMPRESS=1
   export CCACHE_DIR=/nix/var/cache/ccache-kernel
   export CCACHE_UMASK=007
-
-  ccache_wrap() {
-    local real_compiler="$1"
-    local wrapper_path="$2"
-    printf "#!/bin/sh\nreal_compiler='%s'\n%s" "$real_compiler" "${wrapperBody}" > "$wrapper_path"
-    chmod +x "$wrapper_path"
-  }
-
+  source ${./cachewrp}
   mkdir -p $TMPDIR/ccache-wrappers
 
   ${
