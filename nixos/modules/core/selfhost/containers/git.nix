dbfeec4c429f9d5de4068813ee31f1729076aa20
victor7w7r <arkano036@gmail.com>
2026-04-21 13:07:04 -0500

    fix: add git move virt

--- nixos/modules/core/selfhost/containers/git.nix
+++ nixos/modules/core/selfhost/containers/git.nix
@@ -55,40 +55,41 @@
             ];
           };
         };
-      };
 
-    virtualisation = {
-      docker = {
-        enable = true;
-        daemon.settings = {
-          "bridge" = "none";
-          "storage-driver" = "vfs";
-          dns = [
-            "8.8.8.8"
-            "1.1.1.1"
-          ];
-        };
-      };
-
-      oci-containers = {
-        backend = "docker";
-        containers.onedev = {
-          image = "1dev/server";
-          autoStart = true;
-          ports = [
-            "6610:6610"
-            "6611:6611"
-          ];
-          environment = {
-            # initial_server_url = "https://${builtins.toString networkConfig.publicIp}/onedev/";
+        virtualisation = {
+          docker = {
+            enable = true;
+            daemon.settings = {
+              "bridge" = "none";
+              "storage-driver" = "vfs";
+              dns = [
+                "8.8.8.8"
+                "1.1.1.1"
+              ];
+            };
           };
-          extraOptions = [ "--network=host" ];
-          volumes = [
-            "onedev:/opt/onedev"
-            "/var/run/docker.sock:/var/run/docker.sock"
-          ];
+
+          oci-containers = {
+            backend = "docker";
+            containers.onedev = {
+              image = "1dev/server";
+              autoStart = true;
+              ports = [
+                "6610:6610"
+                "6611:6611"
+              ];
+              environment = {
+                # initial_server_url = "https://${builtins.toString networkConfig.publicIp}/onedev/";
+              };
+              extraOptions = [ "--network=host" ];
+              volumes = [
+                "onedev:/opt/onedev"
+                "/var/run/docker.sock:/var/run/docker.sock"
+              ];
+            };
+          };
         };
       };
-    };
+
   };
 }
