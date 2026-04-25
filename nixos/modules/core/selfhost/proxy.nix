8ec3096b34021e95e3d50b52decaf356768d0a62
victor7w7r <arkano036@gmail.com>
2026-04-25 18:20:10 -0500

    fix: remove headers

--- nixos/modules/core/selfhost/proxy.nix
+++ nixos/modules/core/selfhost/proxy.nix
@@ -18,17 +18,9 @@
         locations = {
           "/" = {
             proxyPass = "https://127.0.0.1:8006";
-            proxy_setHeaders = [
-              "Host $host"
-              "X-Real-IP $remote_addr"
-            ];
           };
           "/pc" = {
             proxyPass = "http://127.0.0.1:9090";
-            proxy_setHeaders = [
-              "Host $host"
-              "X-Real-IP $remote_addr"
-            ];
           };
           "/chat" = {
             proxyPass = "http://10.10.0.4:8080";
@@ -48,24 +40,12 @@
         locations = {
           "/" = {
             proxyPass = "http://10.10.0.2:80";
-            proxy_setHeaders = [
-              "Host $host"
-              "X-Real-IP $remote_addr"
-            ];
           };
           "/git" = {
             proxyPass = "http://10.10.0.3:6610";
-            proxy_setHeaders = [
-              "Host $host"
-              "X-Real-IP $remote_addr"
-            ];
           };
           "/ssh" = {
             proxyPass = "http://10.10.0.3:6611";
-            proxy_setHeaders = [
-              "Host $host"
-              "X-Real-IP $remote_addr"
-            ];
           };
         };
       };
@@ -82,26 +62,14 @@
         locations = {
           "/" = {
             proxyPass = "http://10.10.0.3:8080";
-            proxy_setHeaders = [
-              "Host $host"
-              "X-Real-IP $remote_addr"
-            ];
           };
           "/db" = {
             #TCP TUNNEL
             proxyPass = "http://10.10.0.3:5984";
-            proxy_setHeaders = [
-              "Host $host"
-              "X-Real-IP $remote_addr"
-            ];
           };
           "/mc" = {
             #TCP TUNNEL
             proxyPass = "http://10.10.0.5:25565";
-            proxy_setHeaders = [
-              "Host $host"
-              "X-Real-IP $remote_addr"
-            ];
           };
         };
       };
