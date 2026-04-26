378fea4bbc46834006440607cfdcb8e3f34397ac
victor7w7r <arkano036@gmail.com>
2026-04-26 13:07:23 -0500

    fix: add proxy extraconfig

--- nixos/modules/core/selfhost/proxy.nix
+++ nixos/modules/core/selfhost/proxy.nix
@@ -3,6 +3,8 @@
   services.nginx = {
     enable = true;
     recommendedProxySettings = true;
+    recommendedGzipSettings = true;
+    recommendedOptimisation = true;
 
     virtualHosts = {
       "funnel" = {
@@ -21,6 +23,17 @@
           };
           "/pc" = {
             proxyPass = "http://127.0.0.1:9090";
+            extraConfig = ''
+              proxy_set_header Host $host;
+              proxy_set_header X-Real-IP $remote_addr;
+              proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+              proxy_set_header X-Forwarded-Proto $scheme;
+
+              proxy_set_header Upgrade $http_upgrade;
+              proxy_set_header Connection "upgrade";
+
+              proxy_buffering off;
+            '';
           };
           "/chat" = {
             proxyPass = "http://10.10.0.4:8080";
