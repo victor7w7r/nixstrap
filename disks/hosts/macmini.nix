10b243ab415968bd3e25a107ae02d3b95af53b79
victor7w7r <arkano036@gmail.com>
2026-04-27 16:25:02 -0500

    fx: add none

--- disks/hosts/macmini.nix
+++ disks/hosts/macmini.nix
@@ -35,7 +35,7 @@ let
       priority = 6;
       content =  {
         name = "persistlog";
-        type = "8300";
+        type = "none";
         size = "100%";
       };
     };
@@ -46,7 +46,7 @@ let
       priority = 7;
       content =  {
         name = "storagelog";
-        type = "8300";
+        type = "none";
         size = "100%";
       };
     };
@@ -58,7 +58,7 @@ let
       postCreate = "make-bcache -C /dev/mapper/persistcache";
       content =  {
         name = "persistcache";
-        type = "8300";
+        type = "none";
         size = "100%";
       };
     };
@@ -70,7 +70,7 @@ let
       postCreate = "make-bcache -C /dev/mapper/storagecache";
       content =  {
         name = "storagecache";
-        type = "8300";
+        type = "none";
         size = "100%";
       };
     };
