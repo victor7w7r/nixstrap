c4d7385ca0158eb4019f05bd9b49a7387d26d1d1
victor7w7r <arkano036@gmail.com>
2026-07-03 09:44:47 -0500

    fix: comment icon

--- modules/aspects/dm/plasma/appearance.nix
+++ modules/aspects/dm/plasma/appearance.nix
@@ -1,4 +1,3 @@
-{ self, ... }:
 {
   den.aspects.plasma.appearance.provides.to-users.homeManager = {
     programs.plasma = {
@@ -63,7 +62,7 @@
       };
     };
 
-    home.file.".face.icon".source = "${self}/assets/avatar.png";
+    #home.file.".face.icon".source = "${self}/assets/avatar.png";
 
     qt = {
       enable = true;
