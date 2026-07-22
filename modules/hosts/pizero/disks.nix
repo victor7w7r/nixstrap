930efb05f854384294d9ab73904f08f40458daaa
victor7w7r <arkano036@gmail.com>
2026-07-21 19:03:40 -0500

    fix: persist data

--- modules/hosts/pizero/disks.nix
+++ modules/hosts/pizero/disks.nix
@@ -1,33 +1,36 @@
 { disko, ... }:
 {
-  den.aspects.pizero.disks.nixos.disko.devices = with disko; {
-    disk = {
-      root = ephemeral.root { };
-      main = disk.gpt {
-        device = "mmcblk0";
-        partitions = {
-          esp = esp.call { size = "500M"; };
-          persist = f2fs.call {
-            name = "persist";
-            size = "100%";
-            mountpoint = "/nix/persist";
-            priority = 2;
+  den.aspects.pizero.disks.nixos = {
+    fileSystems."/nix/persist".neededForBoot = true;
+    disko.devices = with disko; {
+      disk = {
+        root = ephemeral.root { };
+        main = disk.gpt {
+          device = "mmcblk0";
+          partitions = {
+            esp = esp.call { size = "500M"; };
+            persist = f2fs.call {
+              name = "persist";
+              size = "100%";
+              mountpoint = "/nix/persist";
+              priority = 2;
+            };
           };
         };
-      };
-      sda = disk.gpt {
-        device = "sda";
-        partitions = {
-          swapcrypt = luks.call {
-            name = "swapcrypt";
-            size = "4G";
-            content = swap.call { };
-            priority = 1;
-          };
-          system = xfs.call {
-            name = "system";
-            size = "100%";
-            mountpoint = "/nix";
+        sda = disk.gpt {
+          device = "sda";
+          partitions = {
+            swapcrypt = luks.call {
+              name = "swapcrypt";
+              size = "4G";
+              content = swap.call { };
+              priority = 1;
+            };
+            system = xfs.call {
+              name = "system";
+              size = "100%";
+              mountpoint = "/nix";
+            };
           };
         };
       };
