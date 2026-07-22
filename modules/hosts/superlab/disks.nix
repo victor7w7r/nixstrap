a31bdee51f9b3cb8253ecb123bcc3dd9cfeec668
victor7w7r <arkano036@gmail.com>
2026-07-22 15:55:54 -0500

    fix: superlab etc persist

--- modules/hosts/superlab/disks.nix
+++ modules/hosts/superlab/disks.nix
@@ -1,23 +1,26 @@
 { disko, ... }:
 {
-  den.aspects.superlab.disks.nixos.disko.devices.disk = with disko; {
-    root = ephemeral.root { };
-    main = disk.gpt {
-      device = "nvme0n1";
-      partitions = {
-        esp = esp.call { size = "256M"; };
-        swapcrypt = luks.call {
-          name = "swapcrypt";
-          size = "32G";
-          content = swap.call { };
-          priority = 1;
-        };
-        system = btrfs.call {
-          name = "system";
-          size = "100%";
-          priority = 2;
-          subvolumes = disko.btrfs.subvolumes {
-            hasEtc = true;
+  den.aspects.superlab.disks.nixos.disko = {
+    fileSystems."/etc".neededForBoot = true;
+    devices.disk = with disko; {
+      root = ephemeral.root { };
+      main = disk.gpt {
+        device = "nvme0n1";
+        partitions = {
+          esp = esp.call { size = "256M"; };
+          swapcrypt = luks.call {
+            name = "swapcrypt";
+            size = "32G";
+            content = swap.call { };
+            priority = 1;
+          };
+          system = btrfs.call {
+            name = "system";
+            size = "100%";
+            priority = 2;
+            subvolumes = disko.btrfs.subvolumes {
+              hasEtc = true;
+            };
           };
         };
       };
