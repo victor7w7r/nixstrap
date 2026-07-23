a29c7ac67cd127a722d7f804d692102f350ec57c
victor7w7r <arkano036@gmail.com>
2026-07-23 08:35:20 -0500

    fix: image typos

--- modules/lib/sdcard/image.nix
+++ modules/lib/sdcard/image.nix
@@ -3,17 +3,17 @@
     mkdir -p $out
 
     echo "Creating sdcard partition map..."
+    gap=8
     bootSizeMB=${toString bootSize}
     nextPartSizeMB=${toString nextPartSize}
     swapSizeMB=${if isEntireDisk then "32768" else "0"}
 
     bootImgSize=$(( (gap + bootSizeMB + swapSizeMB + nextPartSizeMB) * 1024 * 1024 + 16 * 1024 * 1024 ))
     truncate -s $bootImgSize boot.img
-    gap=8
 
     sfdisk --no-reread --no-tell-kernel boot.img <<EOF
-      label: "gpt"
-      label-id: "2178694E-0000-4000-8000-000000000000"
+      label: gpt
+      label-id: 2178694E-0000-4000-8000-000000000000
       start=''${gap}M, size=''${bootSizeMB}M, type=C12A7328-F81F-11D2-BA4B-00A0C93EC93B, name="disk-main-esp", attrs="esp"
       ${lib.optionalString isEntireDisk ''
         start=$((gap + bootSizeMB))M, size=''${swapSizeMB}M, type=0657FA6D-A451-463C-B3A4-000000000000, name="disk-main-swapcrypt"
