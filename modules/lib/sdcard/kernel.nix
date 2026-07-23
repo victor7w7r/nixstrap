03dbe653ea3f62fca8e5650977ed169a213df1c4
victor7w7r <arkano036@gmail.com>
2026-07-22 19:48:35 -0500

    fix: sdcard uboot sunxi with other branch

--- modules/lib/sdcard/kernel.nix
+++ modules/lib/sdcard/kernel.nix
@@ -8,12 +8,30 @@
         (pkgs.buildUBoot {
           defconfig = "orangepi_zero2w_defconfig";
           extraMeta.platforms = [ "aarch64-linux" ];
-          nativeBuildInputs = [
-            pkgs.buildPackages.bison
-            pkgs.buildPackages.flex
-          ];
-          BL31 = "${pkgs.armTrustedFirmwareAllwinnerH616}/bl31.bin";
           filesToInstall = [ "u-boot-sunxi-with-spl.bin" ];
+          version = "2024.04";
+          src = pkgs.fetchurl {
+            url = "https://ftp.denx.de/pub/u-boot/u-boot-2024.04.tar.bz2";
+            hash = "sha256-GKhT/jn6160DqQzC1Cda6u1tppc13vrDSSuAUIhD3Uo=";
+          };
+
+          nativeBuildInputs = [
+            pkgs.dtc
+            pkgs.armTrustedFirmwareTools
+            pkgs.bison
+            pkgs.flex
+            pkgs.which
+            pkgs.swig
+            pkgs.openssl
+
+            (pkgs.python3.withPackages (p: [
+              p.setuptools
+              p.libfdt
+              p.pyelftools
+            ]))
+          ];
+
+          BL31 = "${pkgs.armTrustedFirmwareAllwinner}/bl31.bin";
         })
         |> (uboot: "dd if=${uboot}/u-boot-sunxi-with-spl.bin of=$bootImg bs=1024 seek=8 conv=notrunc")
       else
