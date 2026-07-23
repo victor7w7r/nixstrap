caa390d64efd99741bbfc56445b6d8225ed8b047
victor7w7r <arkano036@gmail.com>
2026-07-22 20:49:59 -0500

    fix: add seed properties

--- modules/lib/sdcard/store.nix
+++ modules/lib/sdcard/store.nix
@@ -52,13 +52,8 @@
     }
 
     echo "Creating and compressing store partition..."
-    systemd-repart \
-      --root=/ \
-      --dry-run=no \
-      --empty=create \
-      --size=auto \
-      --definitions=./repart.d \
-      store.img
+    systemd-repart --root=/ --dry-run=no --empty=create --seed=00000000-0000-0000-0000-000000000000 \
+      --size=auto --definitions=./repart.d --copy-source=/ store.img
     ${
       if isEntireDisk then
         ''
