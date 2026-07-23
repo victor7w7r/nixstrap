e4c8a8ff8be1e93749dba701117501fea0bbce4d
victor7w7r <arkano036@gmail.com>
2026-07-22 20:11:12 -0500

    fix: return uboot

--- modules/lib/sdcard/kernel.nix
+++ modules/lib/sdcard/kernel.nix
@@ -8,45 +8,9 @@
         (pkgs.buildUBoot {
           defconfig = "orangepi_zero2w_defconfig";
           extraMeta.platforms = [ "aarch64-linux" ];
+          env.BL31 = "${pkgs.armTrustedFirmwareAllwinnerH616}/bl31.bin";
           filesToInstall = [ "u-boot-sunxi-with-spl.bin" ];
-          version = "2024.04";
-          src = pkgs.fetchurl {
-            url = "https://ftp.denx.de/pub/u-boot/u-boot-2024.04.tar.bz2";
-            hash = "sha256-GKhT/jn6160DqQzC1Cda6u1tppc13vrDSSuAUIhD3Uo=";
-          };
-
-          BL31 = "${pkgs.armTrustedFirmwareAllwinner}/bl31.bin";
-        }).overrideAttrs
-          (
-            oldAttrs:
-            let
-              pyEnv = pkgs.python3.withPackages (
-                p: with p; [
-                  setuptools
-                  pyelftools
-                  libfdt
-                ]
-              );
-            in
-            {
-              nativeBuildInputs = (oldAttrs.nativeBuildInputs or [ ]) ++ [
-                pkgs.bc
-                pkgs.dtc
-                pkgs.armTrustedFirmwareTools
-                pkgs.bison
-                pkgs.flex
-                pkgs.which
-                pkgs.swig
-                pkgs.openssl
-                pyEnv
-              ];
-
-              preBuild = (oldAttrs.preBuild or "") + ''
-                export PYTHON="${pyEnv}/bin/python3"
-                export PYTHONPATH="${pyEnv}/${pkgs.python3.sitePackages}:$PYTHONPATH"
-              '';
-            }
-          )
+        })
         |> (uboot: "dd if=${uboot}/u-boot-sunxi-with-spl.bin of=$bootImg bs=1024 seek=8 conv=notrunc")
       else
         ""
