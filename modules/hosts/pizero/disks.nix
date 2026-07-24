d375af81a31897ab4c590405ee5bf19ba54f9c79
victor7w7r <arkano036@gmail.com>
2026-07-24 13:21:27 -0500

    fix: services to stop

--- modules/hosts/pizero/disks.nix
+++ modules/hosts/pizero/disks.nix
@@ -2,6 +2,18 @@
 {
   den.aspects.pizero.disks.nixos = {
     fileSystems."/nix/persist".neededForBoot = true;
+    boot.initrd = {
+      systemd.settings = {
+        Manager = {
+          DefaultTimeoutStartSec = "30s";
+          DefaultTimeoutStopSec = "30s";
+        };
+      };
+      systemd.units."systemd-cryptsetup@*.service".text = ''
+        [Job]
+        JobTimeoutSec=30s
+      '';
+    };
     disko.devices = with disko; {
       disk = {
         root = ephemeral.root { };
