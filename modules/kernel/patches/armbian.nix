fa92fa96fea971fd966c1189da045d72171b8991
victor7w7r <arkano036@gmail.com>
2026-07-28 17:53:04 -0500

    fix: backer mixer

--- modules/kernel/patches/armbian.nix
+++ modules/kernel/patches/armbian.nix
@@ -35,6 +35,7 @@
                 sed -i -E '/.*(drm-prime-limit-scatter-list-size-with-dedicated-dma-device).*/d' series.conf
                 sed -i -E '/.*(drv-iommu-sunxi-add-iommu-driver).*/d' series.conf
                 sed -i -E '/.*(drm-gem-dma-support-dedicated-dma-device-for-allocation-and-mapping).*/d' series.conf
+                sed -i -E '/.*(drm-sun4i-use-backend-mixer-as-dedicated-dma-device).*/d' series.conf
                 #sed -i -E '/.*(drv-net-stmmac-dwmac-sun8i-add-h616-internal-phy).*/d' series.conf
               '';
           installPhase = "cp series.conf $out";
