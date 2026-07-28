503670cf46cb431ebac9e5494e03ed8d5a1de73a
victor7w7r <arkano036@gmail.com>
2026-07-28 17:59:43 -0500

    fix: DE33

--- modules/kernel/patches/armbian.nix
+++ modules/kernel/patches/armbian.nix
@@ -30,14 +30,13 @@
             else
               ''
                 cp $src/patch/kernel/archive/sunxi-7.0/series.conf ./series.conf
-                sed -i -E '/.*(cw1200-7.0|opi3-eth-7.0|tcpm-7.0|a83t-suspend-7.0|DE33|net-usb|arm64-dts-sun50i-h6-|drv-spidev-).*/d' series.conf
+                sed -i -E '/.*(cw1200-7.0|opi3-eth-7.0|tcpm-7.0|a83t-suspend-7.0|net-usb|arm64-dts-sun50i-h6-|drv-spidev-).*/d' series.conf
                 sed -i -E '/.*(arm64-xor-Select-32regs-without-benchmark-to-speed-u).*/d' series.conf
-                sed -i -E '/.*(drm-prime-limit-scatter-list-size-with-dedicated-dma-device).*/d' series.conf
-                sed -i -E '/.*(drv-iommu-sunxi-add-iommu-driver).*/d' series.conf
                 sed -i -E '/.*(drm-gem-dma-support-dedicated-dma-device-for-allocation-and-mapping).*/d' series.conf
+                sed -i -E '/.*(drm-prime-limit-scatter-list-size-with-dedicated-dma-device).*/d' series.conf
                 sed -i -E '/.*(drm-sun4i-use-backend-mixer-as-dedicated-dma-device).*/d' series.conf
+                sed -i -E '/.*(drv-iommu-sunxi-add-iommu-driver).*/d' series.conf
                 sed -i -E '/.*(drv-media-dvb-frontends-si2168-fix-cmd-timeout).*/d' series.conf
-                #sed -i -E '/.*(drv-net-stmmac-dwmac-sun8i-add-h616-internal-phy).*/d' series.conf
               '';
           installPhase = "cp series.conf $out";
         })
