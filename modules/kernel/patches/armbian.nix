c893c0b0bc005aa774198f302314aa0996531a4a
victor7w7r <arkano036@gmail.com>
2026-07-28 16:25:11 -0500

    fix: remove helios patch in all

--- modules/kernel/patches/armbian.nix
+++ modules/kernel/patches/armbian.nix
@@ -18,8 +18,7 @@
         ];
         buildPhase = ''
           find $src/patch/kernel/archive/rockchip64-7.2 -maxdepth 1 -name '*.patch' -printf '%f\n' | sort > series.conf
-          sed -i '/^rk3399-/d' series.conf
-          sed -i '/add-board-helios64.patch/d' series.conf
+          sed -i -E '/.*(rk3399|helios64).*/d' series.conf
         '';
         installPhase = "cp series.conf $out";
       };
