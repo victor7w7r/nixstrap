a19a856e91d7414511c97b4bc69b7e3b4ec92e0c
victor7w7r <arkano036@gmail.com>
2026-07-29 13:44:10 -0500

    fix: post patch in build linux

--- modules/kernel/lib/linux.nix
+++ modules/kernel/lib/linux.nix
@@ -18,41 +18,17 @@
       dtbMake ? "",
       extra ? "",
     }:
-    (pkgs.stdenvNoCC.mkDerivation {
-      name = "linux-mod-src";
-      src =
-        (pkgs.linuxKernel.kernels.linux_7_1.override {
-          argsOverride = {
-            src = pkgs.fetchurl {
-              url = "mirror://kernel/linux/kernel/v7.x/linux-7.1.5.tar.xz";
-              sha256 = "sha256-IqAZazy83zTcJ7d1YfTQQFhf00R+3JqzUxoax54wQec=";
-            };
-            version = "7.1.5";
-            modDirVersion = "7.1.5";
-          };
-        }).src;
-
-      dontBuild = true;
-      dontConfigure = true;
-
-      postPatch = ''
-        ${lib.optionalString (class != "") ''
-          find "./arch/arm64/boot/dts" -mindepth 1 -maxdepth 1 -type d ! -name "${class}" -exec rm -rf {} +
-          cat <<EOF > "./arch/arm64/boot/dts/Makefile"
-              subdir-y += ${class}
-          EOF
-          cat <<EOF > "./arch/arm64/boot/dts/${class}/Makefile"
-              ${dtbMake}
-          EOF
-        ''}
-        ${extra}
-      '';
-      installPhase = ''
-        mkdir -p $out
-        cp -r . $out/
-      '';
-
+    ((pkgs.linuxKernel.kernels.linux_7_1.override {
+      argsOverride = {
+        src = pkgs.fetchurl {
+          url = "mirror://kernel/linux/kernel/v7.x/linux-7.1.5.tar.xz";
+          sha256 = "sha256-HGOSKhGWddOOOuD49u4H8VxBp4arntZlY3SbuMmgji4=";
+        };
+        version = "7.1.4";
+        modDirVersion = "7.1.4";
+      };
     }).src
+    )
     |> (
       src:
       pkgs.buildLinux {
@@ -69,6 +45,19 @@
           name = baseNameOf (toString file);
           patch = file;
         }) patches;
+
+        postPatch = ''
+          ${lib.optionalString (class != "") ''
+            find "./arch/arm64/boot/dts" -mindepth 1 -maxdepth 1 -type d ! -name "${class}" -exec rm -rf {} +
+            cat <<EOF > "./arch/arm64/boot/dts/Makefile"
+                subdir-y += ${class}
+            EOF
+            cat <<EOF > "./arch/arm64/boot/dts/${class}/Makefile"
+                ${dtbMake}
+            EOF
+          ''}
+          ${extra}
+        '';
 
         extraConfig =
           with lib;
