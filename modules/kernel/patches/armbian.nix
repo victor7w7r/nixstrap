485e85729ff6295373930bae2297f2d5d276f741
victor7w7r <arkano036@gmail.com>
2026-07-29 12:22:01 -0500

    fix: remove sound patches sunxo

--- modules/kernel/patches/armbian.nix
+++ modules/kernel/patches/armbian.nix
@@ -31,11 +31,12 @@
               ''
                 cp $src/patch/kernel/archive/sunxi-7.0/series.conf ./series.conf
                 sed -i -E '/.*(rk3399|rk356x|cw1200-7.0|opi3-eth-7.0|tcpm-7.0|a83t-suspend-7.0|net-usb|arm64-dts-sun50i-h6-|drv-spidev-).*/d' series.conf
+                sed -i -E '/.*(arm64-dts-sun50i-h616-add-digital-audio-node).*/d' series.conf
+                sed -i -E '/.*(arm64-dts-sun50i-h616-orangepi-zero-enable-sound).*/d' series.conf
                 sed -i -E '/.*(arm64-xor-Select-32regs-without-benchmark-to-speed-u).*/d' series.conf
-                sed -i -E '/.*(drm-gem-dma-support-dedicated-dma-device-for-allocation-and-mapping).*/d' series.conf
                 sed -i -E '/.*(drm-prime-limit-scatter-list-size-with-dedicated-dma-device).*/d' series.conf
+                sed -i -E '/.*(drm-gem-dma-support-dedicated-dma-device-for-allocation-and-mapping).*/d' series.conf
                 sed -i -E '/.*(drm-sun4i-use-backend-mixer-as-dedicated-dma-device).*/d' series.conf
-                sed -i -E '/.*(drv-iommu-sunxi-add-iommu-driver).*/d' series.conf
                 sed -i -E '/.*(drv-media-dvb-frontends-si2168-fix-cmd-timeout).*/d' series.conf
               '';
           installPhase = "cp series.conf $out";
