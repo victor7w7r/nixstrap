f9e460a5f71acdee7cf17d1e07982753885462ca
victor7w7r <arkano036@gmail.com>
2026-07-29 13:16:32 -0500

    fix: change to a derivation

--- modules/kernel/lib/linux.nix
+++ modules/kernel/lib/linux.nix
@@ -18,30 +18,41 @@
       dtbMake ? "",
       extra ? "",
     }:
-    ((pkgs.linuxKernel.kernels.linux_7_1.override {
-      argsOverride = {
-        src = pkgs.fetchurl {
-          url = "mirror://kernel/linux/kernel/v7.x/linux-7.1.4.tar.xz";
-          sha256 = "sha256-HGOSKhGWddOOOuD49u4H8VxBp4arntZlY3SbuMmgji4=";
-        };
-        version = "7.1.4";
-        modDirVersion = "7.1.4";
-        postPatch = ''
-          ${lib.optionalString (class != "") ''
-            find "./arch/arm64/boot/dts" -mindepth 1 -maxdepth 1 -type d ! -name "${class}" -exec rm -rf {} +
-            cat <<EOF > "./arch/arm64/boot/dts/Makefile"
-                subdir-y += ${class}
-            EOF
-            cat <<EOF > "./arch/arm64/boot/dts/${class}/Makefile"
-                ${dtbMake}
-            EOF
-          ''}
-          ${extra}
-        '';
-      };
-    }).src
+    (pkgs.stdenvNoCC.mkDerivation {
+      name = "linux-mod";
+      src =
+        (pkgs.linuxKernel.kernels.linux_7_1.override {
+          argsOverride = {
+            src = pkgs.fetchurl {
+              url = "mirror://kernel/linux/kernel/v7.x/linux-7.1.4.tar.xz";
+              sha256 = "sha256-HGOSKhGWddOOOuD49u4H8VxBp4arntZlY3SbuMmgji4=";
+            };
+            version = "7.1.4";
+            modDirVersion = "7.1.4";
+          };
+        }).src;
 
-    )
+      dontBuild = true;
+      dontConfigure = true;
+
+      postPatch = ''
+        ${lib.optionalString (class != "") ''
+          find "./arch/arm64/boot/dts" -mindepth 1 -maxdepth 1 -type d ! -name "${class}" -exec rm -rf {} +
+          cat <<EOF > "./arch/arm64/boot/dts/Makefile"
+              subdir-y += ${class}
+          EOF
+          cat <<EOF > "./arch/arm64/boot/dts/${class}/Makefile"
+              ${dtbMake}
+          EOF
+        ''}
+        ${extra}
+      '';
+      installPhase = ''
+        mkdir -p $out
+        cp -r . $out/
+      '';
+
+    }).src
     |> (
       src:
       pkgs.buildLinux {
@@ -94,6 +105,7 @@
         ];
       }
     )
+
     |> (base: {
       kernel = base;
       config = kernel.lib.filtered-config pkgs base.configfile;
