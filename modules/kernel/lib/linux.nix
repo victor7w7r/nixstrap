0781356d9a578c085592c0054f6c38d0c665eda1
victor7w7r <arkano036@gmail.com>
2026-07-29 13:33:58 -0500

    fix: src data

--- modules/kernel/lib/linux.nix
+++ modules/kernel/lib/linux.nix
@@ -19,16 +19,16 @@
       extra ? "",
     }:
     (pkgs.stdenvNoCC.mkDerivation {
-      name = "linux-mod";
+      name = "linux-mod-src";
       src =
         (pkgs.linuxKernel.kernels.linux_7_1.override {
           argsOverride = {
             src = pkgs.fetchurl {
-              url = "mirror://kernel/linux/kernel/v7.x/linux-7.1.4.tar.xz";
-              sha256 = "sha256-HGOSKhGWddOOOuD49u4H8VxBp4arntZlY3SbuMmgji4=";
+              url = "mirror://kernel/linux/kernel/v7.x/linux-7.1.5.tar.xz";
+              sha256 = "sha256-HGOSKhGWddOAAuD49u4H8VxBp4arntZlY3SbuMmgji4=";
             };
-            version = "7.1.4";
-            modDirVersion = "7.1.4";
+            version = "7.1.5";
+            modDirVersion = "7.1.5";
           };
         }).src;
 
