2c2d3623a2c0b23a5e10652e44f263ecb221b117
victor7w7r <arkano036@gmail.com>
2026-07-29 09:03:49 -0500

    fix: comment ccache

--- modules/kernel/lib/linux.nix
+++ modules/kernel/lib/linux.nix
@@ -31,9 +31,9 @@
       pkgs.buildLinux {
         pname = "linux-v7w7r-${localVer}";
         inherit src;
-        stdenv = pkgs.ccacheStdenv.override {
-          stdenv = (pkgs.callPackage "${inputs.cachyos-kernel.outPath}/helpers.nix" { }).stdenvLLVM;
-        };
+        # stdenv = pkgs.ccacheStdenv.override {
+        stdenv = (pkgs.callPackage "${inputs.cachyos-kernel.outPath}/helpers.nix" { }).stdenvLLVM;
+        # };
         version = (kernel.lib.version pkgs src localVer).final;
         modDirVersion = (kernel.lib.version pkgs src localVer).final;
         ignoreConfigErrors = true;
