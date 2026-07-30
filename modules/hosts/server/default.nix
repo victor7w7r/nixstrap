bb67fd94807bdfa48ba76fcd355ea71c1bb6a567
victor7w7r <arkano036@gmail.com>
2026-07-30 09:17:27 -0500

    fix: add linuxPackages

--- modules/hosts/server/default.nix
+++ modules/hosts/server/default.nix
@@ -69,7 +69,7 @@
                 options kvm-intel nested=1
                 options kvm_intel emulate_invalid_guest_state=0
               '';
-              kernelPackages = pkgs.cachyosKernels.linux-cachyos-latest-lto; # (kernel.hosts.server pkgs).server-kernelPackages;
+              kernelPackages = pkgs.cachyosKernels.linuxPackages-cachyos-latest-lto; # (kernel.hosts.server pkgs).server-kernelPackages;
               swraid = {
                 enable = true;
                 mdadmConf = ''
