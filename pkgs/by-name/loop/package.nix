1987545b73740e2a26a16474fde7bfd92b10389a
victor7w7r <arkano036@gmail.com>
2026-07-31 19:44:04 -0500

    fix: loop cargo lock

--- pkgs/by-name/loop/package.nix
+++ pkgs/by-name/loop/package.nix
@@ -6,5 +6,10 @@
 (crane {
   inherit pkgs;
   pname = "loop";
-  src = inputs.loop;
+  src = pkgs.runCommand "loop-src-with-lock" { } ''
+    mkdir -p $out
+    cp -r ${inputs.loop}/* $out/
+    chmod -R +w $out
+    cp ${./Cargo.lock} $out/Cargo.lock
+  '';
 })
