62d352221b9e5d608a16ec2a8a3a2261f01c3f23
victor7w7r <arkano036@gmail.com>
2026-08-07 15:16:03 -0500

    envelope patches

--- modules/kernel/hosts/phone.nix
+++ modules/kernel/hosts/phone.nix
@@ -10,20 +10,20 @@
       dtbMake = ''
         dtb-\$(CONFIG_ARCH_QCOM) += sdm845-oneplus-enchilada.dtb
         dtb-\$(CONFIG_ARCH_QCOM) += sdm845-oneplus-fajita.dtb
-        dtb-\$(CONFIG_ARCH_QCOM) += sdm845-mtp.dtb
       '';
       patches =
         with kernel.patches.injector pkgs;
-        "${inputs.vanilla-mobile-nixos.outPath}/pkgs/linux-kernel/sdm845/kernel-patches"
-        |>
-          (
+        (
+          "${inputs.vanilla-mobile-nixos.outPath}/pkgs/linux-kernel/sdm845/kernel-patches"
+          |> (
             patches:
             [ "${patches}/../config_fixes.patch" ]
             ++ ((import patches) |> map (item: "${patches}/${item.name}.patch"))
           )
-          ++ cachyos.std
-          ++ tachyon.std
-          ++ bunker.std;
+        )
+        ++ cachyos.std
+        ++ tachyon.std
+        ++ bunker.std;
       structuredExtraConfig = kernel.config.default.phone;
     })
     |> (generated: {
