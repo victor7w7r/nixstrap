8ff18647e8b1b9a616fee40f9bf92316b2979c59
victor7w7r <arkano036@gmail.com>
2026-08-07 11:54:11 -0500

    remove space

--- modules/kernel/lib/arm-wrapper.nix
+++ modules/kernel/lib/arm-wrapper.nix
@@ -26,7 +26,7 @@
 
         ${lib.optionalString (class == "allwinner") ''
           mkdir -p "./drivers/net/wireless/uwe5622"
-          cp -R "${inputs.uwe5622} ''${uwe5622ver#*:}"/{tty-sdio,unisocwcn,unisocwifi,Kconfig,Makefile} "./drivers/net/wireless/uwe5622"
+          cp -R "${inputs.uwe5622}''${uwe5622ver#*:}"/{tty-sdio,unisocwcn,unisocwifi,Kconfig,Makefile} "./drivers/net/wireless/uwe5622"
           echo "obj-\$(CONFIG_SPARD_WLAN_SUPPORT) += uwe5622/" >> ./drivers/net/wireless/Makefile
           sed -i '/source "drivers\/net\/wireless\/ti\/Kconfig"/a source "drivers\/net\/wireless\/uwe5622\/Kconfig"' "./drivers/net/wireless/Kconfig"
           ls drivers/net/wireless/Makefile
