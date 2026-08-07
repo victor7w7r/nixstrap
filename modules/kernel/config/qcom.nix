b2a91d36f7869b22f22cbb86bee7de4ecb89924e
victor7w7r <arkano036@gmail.com>
2026-08-07 17:03:34 -0500

    remove clk 6350

--- modules/kernel/config/qcom.nix
+++ modules/kernel/config/qcom.nix
@@ -419,7 +419,6 @@
         SM_DISPCC_MILOS = no;
         SM_GCC_4450 = no;
         SM_GCC_6125 = no;
-        SM_GCC_6350 = no;
         SM_GCC_6375 = no;
         SM_GCC_7150 = no;
         SM_GCC_8150 = no;
