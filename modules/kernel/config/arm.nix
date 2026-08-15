3fdbf9e2c259da9651c0df3f2edb68cb5553d236
victor7w7r <arkano036@gmail.com>
2026-08-15 14:13:06 -0500

    disable minidump for rockchip

--- modules/kernel/config/arm.nix
+++ modules/kernel/config/arm.nix
@@ -87,6 +87,7 @@
       MEDIA_TUNER_TUA9001 = no;
       MEDIA_TUNER_XC2028 = no;
       REISERFS_FS = no;
+      ROCKCHIP_MINIDUMP = no;
       RTC_DRV_HYM8563 = yes;
       SENSORS_PWM_FAN = yes;
       SND_SOC_ES8316 = module;
