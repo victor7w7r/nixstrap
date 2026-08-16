8b60be0d4a56c759a4891e4f0876961fc0a832c1
victor7w7r <arkano036@gmail.com>
2026-08-15 20:53:36 -0500

    only fix define

--- modules/kernel/patches/patch_drm_exec.py
+++ modules/kernel/patches/patch_drm_exec.py
@@ -1,27 +1,24 @@
-import sys
+import sys, re
 
 path = sys.argv[1]
 with open(path, "r") as f:
     content = f.read()
 
-old_section = """#define drm_exec_until_all_locked(exec)				\\
-	for (void *__drm_exec_retry_ptr; ({			\\
-		__label__ __drm_exec_retry;			\\
-		__drm_exec_retry_ptr = &&__drm_exec_retry;	\\
-		(void)__drm_exec_retry_ptr;			\\
-		__drm_exec_retry:				\\
-		drm_exec_cleanup(exec);				\\
-	});)"""
-
-new_section = """#define drm_exec_until_all_locked(exec)				\\
-	__drm_exec_retry:					\\
-	for (bool __retry = true; __retry; __retry = false)	\\
+new_drm_exec = """#define drm_exec_until_all_locked(exec)				\\
+	__label__ __drm_exec_retry;					\\
+	__drm_exec_retry:						\\
+	for (bool __retry = true; __retry; __retry = false)		\\
 		if (drm_exec_cleanup(exec)) {} else"""
 
-if old_section in content:
-    content = content.replace(old_section, new_section)
+content = re.sub(
+    r'#define drm_exec_until_all_locked\(exec\).*?drm_exec_cleanup\(exec\);\s*\}\);?\)',
+    new_drm_exec,
+    content,
+    flags=re.DOTALL
+)
 
 content = content.replace("goto *__drm_exec_retry_ptr;", "goto __drm_exec_retry;")
+content = content.replace("(void)__drm_exec_retry_ptr;", "")
 
 with open(path, "w") as f:
     f.write(content)
