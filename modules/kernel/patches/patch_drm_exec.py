7d5071e49e3d82dbd8d7f5ab7d3b3318cf19d97f
victor7w7r <arkano036@gmail.com>
2026-08-16 14:05:39 -0500

    setup fix in rockchip

--- modules/kernel/patches/patch_drm_exec.py
+++ modules/kernel/patches/patch_drm_exec.py
@@ -1,4 +1,3 @@
-import re
 import sys
 
 path = sys.argv[1]
@@ -6,20 +5,35 @@ path = sys.argv[1]
 with open(path, "r") as f:
     content = f.read()
 
-new_block = """#define drm_exec_until_all_locked(exec)				\\
+old_until = """#define drm_exec_until_all_locked(exec)				\\
+	for (void *__drm_exec_retry_ptr; ({			\\
+		__label__ __drm_exec_retry;			\\
+		__drm_exec_retry_ptr = &&__drm_exec_retry;	\\
+		(void)__drm_exec_retry_ptr;			\\
+		__drm_exec_retry:				\\
+		drm_exec_cleanup(exec);				\\
+	});)"""
+
+old_retry = """#define drm_exec_retry_on_contention(exec)			\\
+	if (unlikely(drm_exec_is_contended(exec)))		\\
+		goto *__drm_exec_retry_ptr"""
+
+new_until = """#define drm_exec_until_all_locked(exec)				\\
 	for (bool __exec_retry = ({ __label__ __drm_exec_retry; __drm_exec_retry: true; }); \\
 	     __exec_retry; __exec_retry = false)			\\
-		for (; drm_exec_cleanup(exec); )
+		for (; drm_exec_cleanup(exec); )"""
 
-#define drm_exec_retry_on_contention(exec)			\\
+new_retry = """#define drm_exec_retry_on_contention(exec)			\\
 	if (unlikely(drm_exec_is_contended(exec)))		\\
 		goto __drm_exec_retry"""
 
-pattern = r"#define drm_exec_until_all_locked\(exec\).*?#define drm_exec_retry_on_contention\(exec\).*?goto \*__drm_exec_retry_ptr;"
-content = re.sub(pattern, new_block, content, flags=re.DOTALL)
+if old_until in content:
+    content = content.replace(old_until, new_until)
 
-if "goto *__drm_exec_retry_ptr;" in content:
-    content = content.replace("goto *__drm_exec_retry_ptr;", "goto __drm_exec_retry;")
+if old_retry in content:
+    content = content.replace(old_retry, new_retry)
+
+content = content.replace("goto *__drm_exec_retry_ptr;", "goto __drm_exec_retry;")
 
 with open(path, "w") as f:
     f.write(content)
