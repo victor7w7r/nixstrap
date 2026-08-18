e33725ddd3a87924ad625c456f9ff4d829af5aa2
victor7w7r <arkano036@gmail.com>
2026-08-17 20:43:19 -0500

    remove all firmware for amrmbian

--- pkgs/by-name/armbian-firmware/package.nix
+++ pkgs/by-name/armbian-firmware/package.nix
@@ -2,7 +2,7 @@
 stdenvNoCC.mkDerivation {
   pname = "armbian-firmware";
   version = "latest";
-
+  nativeBuildInputs = [ pkgs.findutils ];
   dontBuild = true;
   dontFixup = true;
 
@@ -10,5 +10,7 @@ stdenvNoCC.mkDerivation {
   installPhase = ''
     mkdir -p $out/lib/firmware
     cp -a * $out/lib/firmware/
+    find . -mindepth 1 -maxdepth 1 ! -name 'rockchip*' ! -name '*uwe5622*' -exec rm -rf {} +
+    find -L . -type l -delete
   '';
 }
