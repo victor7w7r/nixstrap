9f97b24e7aac7b5c1fa779d926dccea5f0f04fc8
victor7w7r <arkano036@gmail.com>
2026-08-19 18:54:46 -0500

    remove 0002 sched core

--- modules/kernel/patches/tachyon.nix
+++ modules/kernel/patches/tachyon.nix
@@ -137,7 +137,6 @@
       map (patch: "${inputs.tachyon-patches-legacy}/${patch}.patch") (
         [
           "0001-sched-migrate"
-          "0002-sched-core-add-some-branch-hints-based-on-gcov-analy"
           "0002-sched-migrate"
           "0135-initcall-only-print-non-zero-initcall-debug-to-speed"
           "kdf-boottime"
