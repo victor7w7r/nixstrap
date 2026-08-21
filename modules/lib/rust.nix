a6043afe9ed4aca14e30de033a53b7b706b1ba47
victor7w7r <arkano036@gmail.com>
2026-08-21 10:46:26 -0500

    add rust as lib

---
+++ modules/lib/rust.nix
@@ -0,0 +1,58 @@
+{ inputs, lib, ... }:
+{
+  flake-file.inputs.rust-overlay = {
+    url = "github:oxalica/rust-overlay";
+    inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  _module.args.rustCall =
+    {
+      pkgs,
+      pname,
+      src,
+      nativeBuildInputs ? [ ],
+      buildInputs ? [ ],
+      postInstall ? "",
+      installPhase ? "",
+      cargoHash ? lib.fakeSha256,
+      version ? "latest",
+    }:
+    let
+      customRustToolchain =
+        (pkgs.extend (import inputs.rust-overlay)).rust-bin.stable.latest.default.override
+          {
+            extensions = [
+              "rust-src"
+              "rust-analyzer"
+            ];
+          };
+
+      customRustPlatform = pkgs.makeRustPlatform {
+        cargo = customRustToolchain;
+        rustc = customRustToolchain;
+      };
+    in
+    customRustPlatform.buildRustPackage {
+      inherit
+        pname
+        postInstall
+        version
+        installPhase
+        cargoHash
+        buildInputs
+        src
+        ;
+      nativeBuildInputs =
+        with pkgs;
+        [
+          sccache
+          clang
+          mold
+        ]
+        ++ nativeBuildInputs;
+      SCCACHE_DIR = "/var/cache/sccache";
+      RUSTFLAGS = "-C linker=clang -C link-arg=-fuse-ld=mold -C target-cpu=native";
+      RUSTC_WRAPPER = "${pkgs.sccache}/bin/sccache";
+      doCheck = false;
+    };
+}
