64c111f8b638241766afa7e19bea16a7062ad793
victor7w7r <arkano036@gmail.com>
2026-08-22 18:00:59 -0500

    add overlay for DT

--- modules/kernel/lib/dts-cleaner.nix
+++ modules/kernel/lib/dts-cleaner.nix
@@ -1,10 +1,12 @@
-{
+{ lib, ... }: {
   kernel.lib.dts-cleaner =
     {
       pkgs,
       src,
       class,
       dtbMake,
+      overlays ? null,
+      overlayMake ? null,
     }:
     pkgs.runCommand "dts-cleaner" { } ''
       mkdir -p $out && cp -r ${src}/* $out/ && chmod -R +w $out
@@ -15,5 +17,14 @@
       cat <<EOF > "$out/arch/arm64/boot/dts/${class}/Makefile"
         ${dtbMake}
       EOF
+
+      ${lib.optionalString (overlays != null && overlayMake != null) ''
+        mkdir -p "$out/arch/arm64/boot/dts/${class}/overlay"
+        cp -r ${overlays}/* $out/arch/arm64/boot/dts/${class}/overlay/
+        echo "obj-y += overlay/" >> $out/arch/arm64/boot/dts/${class}/Makefile
+        cat <<EOF > "$out/arch/arm64/boot/dts/${class}/overlay/Makefile"
+          ${overlayMake}
+        EOF
+      ''}
     '';
 }
