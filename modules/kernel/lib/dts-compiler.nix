3d0274eecbaf6c76fabd22dc8d4dec32757b2add
victor7w7r <arkano036@gmail.com>
2026-08-23 08:26:34 -0500

    create overlay

--- modules/kernel/lib/dts-compiler.nix
+++ modules/kernel/lib/dts-compiler.nix
@@ -17,6 +17,7 @@
         find "src/arch/arm64/boot/dts" -mindepth 1 -maxdepth 1 -type d ! -name "${class}" -exec rm -rf {} +
         find "src/arch/arm64/boot/dts/${class}" -maxdepth 1 -type f ! -name '${dtbClass}*' -delete
 
+        mkdir -p "src/arch/arm64/boot/dts/${class}/overlay"
         cp -r ${overlays}/* src/arch/arm64/boot/dts/${class}/overlay/ && chmod -R +w src
         find src/arch/arm64/boot/dts/${class}/overlay -maxdepth 1 -type f ! -name '${overlayClass}-*' -delete
 
