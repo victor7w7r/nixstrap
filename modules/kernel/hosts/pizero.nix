4d8c97c697afad421de50837092bfbcc409a17bc
victor7w7r <arkano036@gmail.com>
2026-08-23 08:22:35 -0500

    delete dtbs calls

--- modules/kernel/hosts/pizero.nix
+++ modules/kernel/hosts/pizero.nix
@@ -43,48 +43,6 @@
             inherit pkgs src;
             class = "allwinner";
             dtbMake = ''dtb-\$(CONFIG_ARCH_SUNXI) += sun50i-h618-orangepi-zero2w.dtb'';
-            overlays = "${inputs.armbian}/patch/kernel/archive/sunxi-6.18/overlay_64";
-            overlayMake = ''
-              dtbo-\$(CONFIG_ARCH_SUNXI) += sun50i-h616-gpu.dtbo
-              dtbo-\$(CONFIG_ARCH_SUNXI) += sun50i-h616-i2c0-pi.dtbo
-              dtbo-\$(CONFIG_ARCH_SUNXI) += sun50i-h616-i2c1-pi.dtbo
-              dtbo-\$(CONFIG_ARCH_SUNXI) += sun50i-h616-i2c2-pi.dtbo
-              dtbo-\$(CONFIG_ARCH_SUNXI) += sun50i-h616-i2c2-ph.dtbo
-              dtbo-\$(CONFIG_ARCH_SUNXI) += sun50i-h616-i2c3-pg.dtbo
-              dtbo-\$(CONFIG_ARCH_SUNXI) += sun50i-h616-i2c3-ph.dtbo
-              dtbo-\$(CONFIG_ARCH_SUNXI) += sun50i-h616-i2c4-pg.dtbo
-              dtbo-\$(CONFIG_ARCH_SUNXI) += sun50i-h616-i2c4-ph.dtbo
-              dtbo-\$(CONFIG_ARCH_SUNXI) += sun50i-h616-keys.dtbo
-              dtbo-\$(CONFIG_ARCH_SUNXI) += sun50i-h616-pwm1-ph3.dtbo
-              dtbo-\$(CONFIG_ARCH_SUNXI) += sun50i-h616-pwm1-pi11.dtbo
-              dtbo-\$(CONFIG_ARCH_SUNXI) += sun50i-h616-pwm2-ph2.dtbo
-              dtbo-\$(CONFIG_ARCH_SUNXI) += sun50i-h616-pwm2-pi12.dtbo
-              dtbo-\$(CONFIG_ARCH_SUNXI) += sun50i-h616-pwm3-ph0.dtbo
-              dtbo-\$(CONFIG_ARCH_SUNXI) += sun50i-h616-pwm3-pi13.dtbo
-              dtbo-\$(CONFIG_ARCH_SUNXI) += sun50i-h616-pwm4-ph1.dtbo
-              dtbo-\$(CONFIG_ARCH_SUNXI) += sun50i-h616-pwm4-pi14.dtbo
-              dtbo-\$(CONFIG_ARCH_SUNXI) += sun50i-h616-uart2-pg.dtbo
-              dtbo-\$(CONFIG_ARCH_SUNXI) += sun50i-h616-uart2-pg-rts-cts.dtbo
-              dtbo-\$(CONFIG_ARCH_SUNXI) += sun50i-h616-uart2-ph.dtbo
-              dtbo-\$(CONFIG_ARCH_SUNXI) += sun50i-h616-uart2-ph-rts-cts.dtbo
-              dtbo-\$(CONFIG_ARCH_SUNXI) += sun50i-h616-uart2-pi.dtbo
-              dtbo-\$(CONFIG_ARCH_SUNXI) += sun50i-h616-uart2-pi-rts-cts.dtbo
-              dtbo-\$(CONFIG_ARCH_SUNXI) += sun50i-h616-uart3-pi.dtbo
-              dtbo-\$(CONFIG_ARCH_SUNXI) += sun50i-h616-uart3-pi-rts-cts.dtbo
-              dtbo-\$(CONFIG_ARCH_SUNXI) += sun50i-h616-uart4-pi.dtbo
-              dtbo-\$(CONFIG_ARCH_SUNXI) += sun50i-h616-uart4-pi-rts-cts.dtbo
-              dtbo-\$(CONFIG_ARCH_SUNXI) += sun50i-h616-uart5.dtbo
-              dtbo-\$(CONFIG_ARCH_SUNXI) += sun50i-h616-spi-spidev.dtbo
-              dtbo-\$(CONFIG_ARCH_SUNXI) += sun50i-h616-spidev0_0.dtbo
-              dtbo-\$(CONFIG_ARCH_SUNXI) += sun50i-h616-spidev1_0.dtbo
-              dtbo-\$(CONFIG_ARCH_SUNXI) += sun50i-h616-spidev1_1.dtbo
-              dtbo-\$(CONFIG_ARCH_SUNXI) += sun50i-h616-spidev1_2.dtbo
-              dtbo-\$(CONFIG_ARCH_SUNXI) += sun50i-h616-ir.dtbo
-              dtbo-\$(CONFIG_ARCH_SUNXI) += sun50i-h616-tft35_spi.dtbo
-              dtbo-\$(CONFIG_ARCH_SUNXI) += sun50i-h616-mcp2515.dtbo
-              dtbo-\$(CONFIG_ARCH_SUNXI) += sun50i-h616-ws2812.dtbo
-              dtbo-\$(CONFIG_ARCH_SUNXI) += sun50i-h616-light.dtbo
-            '';
           }
         );
     });
--- modules/kernel/hosts/superlab.nix
+++ modules/kernel/hosts/superlab.nix
@@ -36,58 +36,6 @@
             inherit pkgs src;
             class = "rockchip";
             dtbMake = ''dtb-\$(CONFIG_ARCH_ROCKCHIP) += rk3588-rock-5b.dtb'';
-            overlays = "${inputs.armbian}/patch/kernel/archive/rockchip64-6.18/overlay";
-            overlayMake = ''
-              dtbo-\$(CONFIG_ARCH_ROCKCHIP) += rockchip-rk3588-fanctrl.dtbo
-              dtbo-\$(CONFIG_ARCH_ROCKCHIP) += rockchip-rk3588-sata0.dtbo
-              dtbo-\$(CONFIG_ARCH_ROCKCHIP) += rockchip-rk3588-sata1.dtbo
-              dtbo-\$(CONFIG_ARCH_ROCKCHIP) += rockchip-rk3588-sata2.dtbo
-              dtbo-\$(CONFIG_ARCH_ROCKCHIP) += rockchip-rk3588-hdmirx.dtbo
-              dtbo-\$(CONFIG_ARCH_ROCKCHIP) += rockchip-rk3588-can0-m0.dtbo
-              dtbo-\$(CONFIG_ARCH_ROCKCHIP) += rockchip-rk3588-can0-m1.dtbo
-              dtbo-\$(CONFIG_ARCH_ROCKCHIP) += rockchip-rk3588-can1-m0.dtbo
-              dtbo-\$(CONFIG_ARCH_ROCKCHIP) += rockchip-rk3588-can1-m1.dtbo
-              dtbo-\$(CONFIG_ARCH_ROCKCHIP) += rockchip-rk3588-can2-m0.dtbo
-              dtbo-\$(CONFIG_ARCH_ROCKCHIP) += rockchip-rk3588-can2-m1.dtbo
-              dtbo-\$(CONFIG_ARCH_ROCKCHIP) += rockchip-rk3588-i2c8-m2.dtbo
-              dtbo-\$(CONFIG_ARCH_ROCKCHIP) += rockchip-rk3588-pwm0-m0.dtbo
-              dtbo-\$(CONFIG_ARCH_ROCKCHIP) += rockchip-rk3588-pwm0-m1.dtbo
-              dtbo-\$(CONFIG_ARCH_ROCKCHIP) += rockchip-rk3588-pwm0-m2.dtbo
-              dtbo-\$(CONFIG_ARCH_ROCKCHIP) += rockchip-rk3588-pwm1-m0.dtbo
-              dtbo-\$(CONFIG_ARCH_ROCKCHIP) += rockchip-rk3588-pwm1-m1.dtbo
-              dtbo-\$(CONFIG_ARCH_ROCKCHIP) += rockchip-rk3588-pwm1-m2.dtbo
-              dtbo-\$(CONFIG_ARCH_ROCKCHIP) += rockchip-rk3588-pwm2-m1.dtbo
-              dtbo-\$(CONFIG_ARCH_ROCKCHIP) += rockchip-rk3588-pwm3-m0.dtbo
-              dtbo-\$(CONFIG_ARCH_ROCKCHIP) += rockchip-rk3588-pwm3-m1.dtbo
-              dtbo-\$(CONFIG_ARCH_ROCKCHIP) += rockchip-rk3588-pwm3-m2.dtbo
-              dtbo-\$(CONFIG_ARCH_ROCKCHIP) += rockchip-rk3588-pwm3-m3.dtbo
-              dtbo-\$(CONFIG_ARCH_ROCKCHIP) += rockchip-rk3588-pwm5-m2.dtbo
-              dtbo-\$(CONFIG_ARCH_ROCKCHIP) += rockchip-rk3588-pwm6-m0.dtbo
-              dtbo-\$(CONFIG_ARCH_ROCKCHIP) += rockchip-rk3588-pwm6-m2.dtbo
-              dtbo-\$(CONFIG_ARCH_ROCKCHIP) += rockchip-rk3588-pwm7-m0.dtbo
-              dtbo-\$(CONFIG_ARCH_ROCKCHIP) += rockchip-rk3588-pwm7-m3.dtbo
-              dtbo-\$(CONFIG_ARCH_ROCKCHIP) += rockchip-rk3588-pwm8-m0.dtbo
-              dtbo-\$(CONFIG_ARCH_ROCKCHIP) += rockchip-rk3588-pwm10-m0.dtbo
-              dtbo-\$(CONFIG_ARCH_ROCKCHIP) += rockchip-rk3588-pwm11-m0.dtbo
-              dtbo-\$(CONFIG_ARCH_ROCKCHIP) += rockchip-rk3588-pwm11-m1.dtbo
-              dtbo-\$(CONFIG_ARCH_ROCKCHIP) += rockchip-rk3588-pwm12-m0.dtbo
-              dtbo-\$(CONFIG_ARCH_ROCKCHIP) += rockchip-rk3588-pwm13-m0.dtbo
-              dtbo-\$(CONFIG_ARCH_ROCKCHIP) += rockchip-rk3588-pwm13-m2.dtbo
-              dtbo-\$(CONFIG_ARCH_ROCKCHIP) += rockchip-rk3588-pwm14-m0.dtbo
-              dtbo-\$(CONFIG_ARCH_ROCKCHIP) += rockchip-rk3588-pwm14-m1.dtbo
-              dtbo-\$(CONFIG_ARCH_ROCKCHIP) += rockchip-rk3588-pwm14-m2.dtbo
-              dtbo-\$(CONFIG_ARCH_ROCKCHIP) += rockchip-rk3588-pwm15-m0.dtbo
-              dtbo-\$(CONFIG_ARCH_ROCKCHIP) += rockchip-rk3588-pwm15-m1.dtbo
-              dtbo-\$(CONFIG_ARCH_ROCKCHIP) += rockchip-rk3588-pwm15-m2.dtbo
-              dtbo-\$(CONFIG_ARCH_ROCKCHIP) += rockchip-rk3588-pwm15-m3.dtbo
-              dtbo-\$(CONFIG_ARCH_ROCKCHIP) += rockchip-rk3588-uart1-m1.dtbo
-              dtbo-\$(CONFIG_ARCH_ROCKCHIP) += rockchip-rk3588-uart3-m1.dtbo
-              dtbo-\$(CONFIG_ARCH_ROCKCHIP) += rockchip-rk3588-uart4-m2.dtbo
-              dtbo-\$(CONFIG_ARCH_ROCKCHIP) += rockchip-rk3588-uart6-m1.dtbo
-              dtbo-\$(CONFIG_ARCH_ROCKCHIP) += rockchip-rk3588-uart7-m2.dtbo
-              dtbo-\$(CONFIG_ARCH_ROCKCHIP) += rockchip-rk3588-uart8-m1.dtbo
-              dtbo-\$(CONFIG_ARCH_ROCKCHIP) += rockchip-rk3588-rkvenc-overlay.dtbo
-            '';
           }
         );
     });
