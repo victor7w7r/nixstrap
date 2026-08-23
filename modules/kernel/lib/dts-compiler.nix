46ccec123eb7a128c2106c04b5c7a0cb69be3256
victor7w7r <arkano036@gmail.com>
2026-08-23 08:30:22 -0500

    update compiler for mixing dts

--- modules/kernel/lib/dts-compiler.nix
+++ modules/kernel/lib/dts-compiler.nix
@@ -21,12 +21,27 @@
         cp -r ${overlays}/* src/arch/arm64/boot/dts/${class}/overlay/ && chmod -R +w src
         find src/arch/arm64/boot/dts/${class}/overlay -maxdepth 1 -type f ! -name '${overlayClass}-*' -delete
 
-        for f in src/arch/arm64/boot/dts/${class}/*.dts src/arch/arm64/boot/dts/${class}/overlay/*.dtso; do
+        for f in src/arch/arm64/boot/dts/${class}/overlay/*.dts src/arch/arm64/boot/dts/${class}/overlay/*.dtso; do
           if [ -f "$f" ]; then
             base=$(basename "$f")
-            name=$(echo "$base" | sed -E 's/\.(dtso|dts)$/\.dtbo/')
-            echo "Compiling: $f -> build/$name"
-            dtc -@ -I dts -O dtb -o "build/$name" "$f" || true
+
+            dts_preprocessed=$(cpp -nostdinc \
+              -I src/include \
+              -I src/arch/arm64/boot/dts \
+              -I src/arch/arm64/boot/dts/${class} \
+              -I src/arch/arm64/boot/dts/${class}/overlay \
+              -undef -D__DTS__ -x assembler-with-cpp "$f")
+
+            if echo "$dts_preprocessed" | grep -q "/plugin/;"; then
+              name=$(echo "$base" | sed -E 's/\.(dtso|dts)$/\.dtbo/')
+              echo "Compiling OVERLAY: $f -> build/$name"
+              echo "$dts_preprocessed" | dtc -@ -I dts -O dtb -o "build/$name" - || true
+            else
+              name=$(echo "$base" | sed -E 's/\.(dtso|dts)$/\.dtb/')
+              echo "Compiling BASE DTB: $f -> build/$name"
+
+              echo "$dts_preprocessed" | dtc -I dts -O dtb -o "build/$name" - || true
+            fi
           fi
         done
 
