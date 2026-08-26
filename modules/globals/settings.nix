ab89b427f999d2af6ba8828b941c5485f246c522
victor7w7r <arkano036@gmail.com>
2026-08-26 11:08:31 -0500

    disable gc for arm in store mobile

--- modules/globals/settings.nix
+++ modules/globals/settings.nix
@@ -18,70 +18,77 @@
     };
   };
 
-  den.default.nixos = { lib, ... }: {
-    system.stateVersion = stateVersion;
+  den.default.nixos =
+    {
+      lib,
+      isPiZero,
+      isSuperlab,
+      ...
+    }:
+    {
+      system.stateVersion = stateVersion;
 
-    documentation = {
-      enable = false;
-      doc.enable = false;
-      info.enable = false;
-      man.enable = false;
-    };
-
-    nixpkgs = {
-      config.allowUnfree = true;
-      flake = {
-        setNixPath = false;
-        setFlakeRegistry = false;
-      };
-    };
-
-    nix = {
-      #package = lib.mkDefault pkgs.lix;
-      settings = flake-config // nix-config;
-
-      gc = {
-        automatic = true;
-        dates = "weekly";
-        options = "--delete-older-than 7d";
+      documentation = {
+        enable = false;
+        doc.enable = false;
+        info.enable = false;
+        man.enable = false;
       };
 
-      optimise = {
-        automatic = true;
-        dates = [ "weekly" ];
+      nixpkgs = {
+        config.allowUnfree = true;
+        flake = {
+          setNixPath = false;
+          setFlakeRegistry = false;
+        };
       };
-    };
 
-    programs = {
-      nix-ld.enable = true;
-      nh = {
+      nix = {
+        #package = lib.mkDefault pkgs.lix;
+        settings = flake-config // nix-config;
+
+        gc = {
+          automatic = (!isPiZero && !isSuperlab);
+          dates = "weekly";
+          options = "--delete-older-than 7d";
+        };
+
+        optimise = {
+          automatic = true;
+          dates = [ "weekly" ];
+        };
+      };
+
+      programs = {
+        nix-ld.enable = true;
+        nh = {
+          enable = true;
+          #clean.enable = true;
+          #clean.extraArgs = --keep 10 --keep-since 5d";
+          #flake = "github:herobrauni/nix";
+          #flake = "/etc/nixos";
+        };
+      };
+
+      system.autoUpgrade = {
         enable = true;
-        #clean.enable = true;
-        #clean.extraArgs = --keep 10 --keep-since 5d";
-        #flake = "github:herobrauni/nix";
-        #flake = "/etc/nixos";
+        # upgrade = false;
+        allowReboot = true;
+        flake = inputs.self.outPath;
+        dates = "04:00";
+        randomizedDelaySec = "30min";
+        fixedRandomDelay = true;
+        flags =
+          (lib.concatMap (input: [
+            "--update-input"
+            input
+          ]) (lib.attrNames inputs))
+          ++ [ "--commit-lock-file" ];
+
+        rebootWindow = {
+          lower = "03:00";
+          upper = "05:00";
+        };
       };
     };
-
-    system.autoUpgrade = {
-      enable = true;
-      # upgrade = false;
-      allowReboot = true;
-      flake = inputs.self.outPath;
-      dates = "04:00";
-      randomizedDelaySec = "30min";
-      fixedRandomDelay = true;
-      flags =
-        (lib.concatMap (input: [
-          "--update-input"
-          input
-        ]) (lib.attrNames inputs))
-        ++ [ "--commit-lock-file" ];
-
-      rebootWindow = {
-        lower = "03:00";
-        upper = "05:00";
-      };
-    };
-  };
 }
