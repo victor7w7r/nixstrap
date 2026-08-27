0d5daa5dac3660e2b85e402167b783ea5a297835
victor7w7r <arkano036@gmail.com>
2026-08-27 18:24:15 -0500

    xfce4 session

--- modules/hosts/server/services.nix
+++ modules/hosts/server/services.nix
@@ -7,7 +7,7 @@
 
         xrdp = {
           enable = true;
-          defaultWindowManager = "${pkgs.dbus}/bin/dbus-run-session ${pkgs.xfce.startxfce4}/bin/startxfce4";
+          defaultWindowManager = "${pkgs.dbus}/bin/dbus-run-session ${pkgs.xfce4-session}/bin/startxfce4";
           openFirewall = true;
         };
 
