2b7f1d6532239b15b4b946bb0adb19fa4eb3d2e2
victor7w7r <arkano036@gmail.com>
2026-08-28 07:51:07 -0500

    setup xfc4-session

--- modules/hosts/server/services.nix
+++ modules/hosts/server/services.nix
@@ -25,7 +25,7 @@
               export NIXOS_OZONE_WL=0
               export XDG_RUNTIME_DIR="/run/user/$(id -u)"
 
-              exec ${pkgs.dbus}/bin/dbus-run-session ${pkgs.xfce.startxfce4}/bin/startxfce4
+              exec ${pkgs.dbus}/bin/dbus-run-session ${pkgs.xfce4-session}/bin/startxfce4
             ''
             |> (session: "exec ${session}");
           openFirewall = true;
