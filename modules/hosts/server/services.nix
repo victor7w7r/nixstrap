0ad63992a6e6f20839d1f26854329b469ce62f98
victor7w7r <arkano036@gmail.com>
2026-08-28 08:58:44 -0500

    put comment code outside

--- modules/hosts/server/services.nix
+++ modules/hosts/server/services.nix
@@ -63,16 +63,17 @@
               export QT_QPA_PLATFORM=xcb
               export NIXOS_OZONE_WL=0
               export XDG_RUNTIME_DIR="/run/user/$(id -u)"
-              #export FONTCONFIG_FILE="/etc/fonts/fonts.conf"
-              #export FONTCONFIG_PATH="/etc/fonts"
-              #export GSETTINGS_BACKEND="keyfile"
-              #export PATH="${env}/bin:$PATH"
-              #export XDG_DATA_DIRS="${env}/share:/run/current-system/sw/share''${XDG_DATA_DIRS:+:$XDG_DATA_DIRS}"
-              #export XDG_CONFIG_DIRS="${env}/etc/xdg:/run/current-system/sw/etc/xdg''${XDG_CONFIG_DIRS:+:$XDG_CONFIG_DIRS}"
+
 
               exec ${pkgs.dbus}/bin/dbus-run-session ${pkgs.xfce4-session}/bin/startxfce4
             ''
             #)
+            #export FONTCONFIG_FILE="/etc/fonts/fonts.conf"
+            #export FONTCONFIG_PATH="/etc/fonts"
+            #export GSETTINGS_BACKEND="keyfile"
+            #export PATH="${env}/bin:$PATH"
+            #export XDG_DATA_DIRS="${env}/share:/run/current-system/sw/share''${XDG_DATA_DIRS:+:$XDG_DATA_DIRS}"
+            #export XDG_CONFIG_DIRS="${env}/etc/xdg:/run/current-system/sw/etc/xdg''${XDG_CONFIG_DIRS:+:$XDG_CONFIG_DIRS}"
             |> (session: "exec ${session}");
           openFirewall = true;
         };
