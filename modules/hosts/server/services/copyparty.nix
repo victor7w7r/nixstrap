df453f2cdef5650742463ff5a443ffdb58d1b5db
victor7w7r <arkano036@gmail.com>
2026-08-31 19:40:40 -0500

    update copyparty http

--- modules/hosts/server/services/copyparty.nix
+++ modules/hosts/server/services/copyparty.nix
@@ -3,7 +3,9 @@
   flake-file.inputs.copyparty.url = "github:9001/copyparty";
 
   den.aspects.server.services.copyparty.nixos = { pkgs, ... }: {
+
     imports = [ inputs.copyparty.nixosModules.default ];
+    networking.firewall.allowedTCPPorts = [ 3923 ];
     nixpkgs.overlays = [ inputs.copyparty.overlays.default ];
     environment.systemPackages = [ pkgs.copyparty ];
     services.copyparty.enable = true;
