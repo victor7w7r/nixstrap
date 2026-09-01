6c4e6d24355659bcc5cdfe1636d0878a159d249b
victor7w7r <arkano036@gmail.com>
2026-09-01 12:39:42 -0500

    remove 0186

--- modules/kernel/patches/qcom.nix
+++ modules/kernel/patches/qcom.nix
@@ -57,6 +57,7 @@
               "0156-media-qcom-camss-csiphy-3ph-Add-Gen2-v1.2.1-MIPI-CSI"
               "0162-arm64-dts-qcom-sdm845-shift-axolotl-Add-actuator-for"
               "0185-arm64-dts-qcom-sdm845-lg-common-Add-camera-flash"
+              "0186-arm64-dts-qcom-sdm845-lg-common-Change-ipa-gsi-loade"
             ]
           )
           |> map (item: "${patches}/${item.name}.patch")
