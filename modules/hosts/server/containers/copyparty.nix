dd38475cafa91a980df2ef6f7940939c6694cbfc
victor7w7r <arkano036@gmail.com>
2026-09-02 17:35:37 -0500

    setup copyparty url

--- modules/hosts/server/containers/copyparty.nix
+++ modules/hosts/server/containers/copyparty.nix
@@ -59,7 +59,7 @@
             no-robots = true;
             theme = 2;
             xff-hdr = "x-forwarded-for";
-            xff-src = "192.168.100.10,100.64.0.1";
+            xff-src = "192.168.100.10,100.64.0.1,100.64.0.10";
           };
           accounts.victor7w7r.passwordFile = config.age.secrets.copyparty-pass.path;
         };
