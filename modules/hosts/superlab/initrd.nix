16cccb050ee20980276298de7416e791cdf23dfa
victor7w7r <arkano036@gmail.com>
2026-09-02 15:08:11 -0500

    reform initrd in superlab

--- modules/hosts/superlab/initrd.nix
+++ modules/hosts/superlab/initrd.nix
@@ -1,30 +1,6 @@
 {
-  den.aspects.superlab.initrd.nixos = { config, pkgs, ... }: {
+  den.aspects.superlab.initrd.nixos = { config, ... }: {
     boot.initrd = {
-      services.udev.packages = [ pkgs.yubikey-personalization ];
-
-      systemd = {
-        enable = true;
-        tpm2.enable = false;
-
-        storePaths =
-          with pkgs;
-          (map
-            (fw: {
-              source = "${config.hardware.firmware}/lib/firmware/${fw}";
-              target = "/extra-firmware/${fw}";
-            })
-            [
-              "rtl_nic/rtl8125b-2.fw"
-              "arm/mali/arch10.8/mali_csffw.bin"
-            ]
-          )
-          ++ [
-            pcsclite.lib
-            libfido2
-            "${config.boot.initrd.systemd.package}/lib/cryptsetup/libcryptsetup-token-systemd-fido2.so"
-          ];
-      };
       kernelModules = [
         "display_connector"
         "dm_crypt"
@@ -42,6 +18,23 @@
         #"resume=${config.boot.resumeDevice}"
       ];
 
+      systemd = {
+        tpm2.enable = false;
+        storePaths =
+          (map
+            (fw: {
+              source = "${config.hardware.firmware}/lib/firmware/${fw}";
+              target = "/extra-firmware/${fw}";
+            })
+            [
+              "rtl_nic/rtl8125b-2.fw"
+              "arm/mali/arch10.8/mali_csffw.bin"
+            ]
+          )
+         ;
+      };
+
+
       luks.devices = {
         /*
           swapcrypt = {
@@ -52,6 +45,7 @@
         system = {
           device = "/dev/disk/by-partlabel/disk-main-system";
           crypttabExtraOpts = [ "fido2-device=auto" ];
+          preLVM = true;
           allowDiscards = true;
         };
       };
