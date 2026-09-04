1d03a6d03470fa7c38739a7849c8f5484283766e
victor7w7r <arkano036@gmail.com>
2026-09-04 10:09:13 -0500

    reorder defconfig clean

--- modules/kernel/lib/kernel-cleaner.nix
+++ modules/kernel/lib/kernel-cleaner.nix
@@ -22,8 +22,6 @@
       patchPhase = ''
         cp ${if config != null then config else "${src}/arch/${arch}/configs/defconfig"} config
 
-        sed -i '/^#/d' config
-
         sed -i '/^CONFIG_CC_/d' config
         sed -i '/^CONFIG_G*CC_/d' config
         sed -i '/^CONFIG_KUNIT$/d' config
@@ -35,29 +33,26 @@
         sed -i '/^CONFIG_.*_PHY=/d' config
         sed -i '/^CONFIG_AF_RXRPC/d' config
         sed -i '/^CONFIG_ALTERA_STAPL/d' config
+        sed -i '/^CONFIG_ATALK/d' config
+        sed -i '/^CONFIG_ATM/d' config
         sed -i '/^CONFIG_COMEDI/d' config
         sed -i '/^CONFIG_CPU_SUP_/d' config
         sed -i '/^CONFIG_DEBUG_/d' config
-        sed -i '/^CONFIG_ENCRYPTED_KEYS/d' config
         sed -i '/^CONFIG_FUSION_/d' config
-        sed -i '/^CONFIG_GPIO_/d' config
         sed -i '/^CONFIG_INFINIBAND_/d' config
         sed -i '/^CONFIG_KEYBOARD_/d' config
-        sed -i '/^CONFIG_PHY_QCOM_USB_HS/d' config
-        sed -i '/^CONFIG_RXKAD/d' config
-        sed -i '/^CONFIG_RDS/d' config
-        sed -i '/^CONFIG_TIPC/d' config
-        sed -i '/^CONFIG_ATM/d' config
-        sed -i '/^CONFIG_LLC2/d' config
-        sed -i '/^CONFIG_ATALK/d' config
-        sed -i '/^CONFIG_X25/d' config
         sed -i '/^CONFIG_LAPB/d' config
+        sed -i '/^CONFIG_LLC2/d' config
         sed -i '/^CONFIG_PHONET/d' config
+        sed -i '/^CONFIG_PHY_QCOM_USB_HS/d' config
+        sed -i '/^CONFIG_RDS/d' config
+        sed -i '/^CONFIG_RXKAD/d' config
         sed -i '/^CONFIG_SCSI_/d' config
         sed -i '/^CONFIG_SND_/d' config
         sed -i '/^CONFIG_SSB/d' config
-        sed -i '/^CONFIG_W1/d' config
-        sed -i '/^CONFIG_W1_/d' config
+        sed -i '/^CONFIG_TIPC/d' config
+        sed -i '/^CONFIG_W1*/d' config
+        sed -i '/^CONFIG_X25/d' config
         sed -i '/^CONFIG_XFRM/d' config
 
         # sed -i '/^CONFIG_IIO/d' config
@@ -72,6 +67,7 @@
         # sed -i '/^CONFIG_SYSTEM/d' config
         # sed -i '/^CONFIG_USB_/d' config
 
+        sed -i '/^#/d' config
         sed -i '/^$/N;/\n$/D' config
 
         mkdir -p $out && cp -r ${src}/* $out/ && chmod -R +w $out
